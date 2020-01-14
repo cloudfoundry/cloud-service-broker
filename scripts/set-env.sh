1df9685dfcaa7cf7fdb5ee1d1a7aacb07ee3c1cc
Ernie Billing <ebilling@pivotal.io>
2020-01-14 09:59:03 -0800

    start db script set-env.sh sets legacy GCP root creds
    Co-authored-by: Sridhar Vennela <svennela@pivotal.io>

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -9,3 +9,4 @@ export AZURE_TENANT_ID=$(lpass show --notes "Shared-CF Platform Engineering/Azur
 export AZURE_CLIENT_ID=$(lpass show --notes "Shared-CF Platform Engineering/Azure Service Account Key" | jq -r .client_id)
 export AZURE_CLIENT_SECRET=$(lpass show --notes "Shared-CF Platform Engineering/Azure Service Account Key" | jq -r .client_secret)
 export GCP_SERVICE_ACCOUNT_JSON=$(lpass show --notes "Shared-CF Platform Engineering/pks cluster management gcp service account")
+export ROOT_SERVICE_ACCOUNT_JSON="${GCP_SERVICE_ACCOUNT_JSON}"
