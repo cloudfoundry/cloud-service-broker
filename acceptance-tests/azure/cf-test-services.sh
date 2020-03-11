61a04c0cb28e1c1f541f76b537f9d73ac713b498
Ernie Billing <ebilling@pivotal.io>
2020-03-10 17:39:54 -0700

    [#171737801] added mongodb acceptance tests, changed mongo plans to lowercase names

--- acceptance-tests/azure/cf-test-services.sh
+++ acceptance-tests/azure/cf-test-services.sh
@@ -9,4 +9,5 @@ cf marketplace
 
 ../../scripts/cf-test-service.sh azure-mysql small
 ../../scripts/cf-test-service.sh azure-redis small
+../../scripts/cf-test-service.sh azure-mongodb small -c '{"db_name": "musicdb", "collection_name": "album", "shard_key": "_id"}'
 
