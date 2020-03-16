6b7ccf42f2aff3fc76ebd335aa1859292c610b37
Ernie Billing <ebilling@pivotal.io>
2020-03-16 11:08:34 -0700

    modularizied service tests

--- acceptance-tests/spring-music-validator/index.js
+++ acceptance-tests/spring-music-validator/index.js
@@ -1,89 +1,43 @@
 var restify = require('restify');
 var vcapServices = require('vcap_services');
-var mysql = require('mysql');
+var testMysql = require('./mysql')
+var testRedis = require('./redis')
 
-// reject with error
-// resolve with connection
-function connectMysql(params) {
-    return new Promise((resolve, reject) => {
-        var con = mysql.createConnection(params)
-        con.connect(err => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(con)
-            }
-        })
-    })
+function runServer(content) {
+    var server = restify.createServer();
+    server.get('/', (_, res, next) => {
+        res.send(content)
+        next()
+    });
+
+    server.listen(process.env.PORT || 8080, function () {
+        console.log('%s listening at %s', server.name, server.url);
+    });
+
 }
 
-function mysqlShowTables(con) {
-    return new Promise((resolve, reject) => {
-        con.query("SHOW tables", (err, result, fields) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve({ tables: result, connection: con })
-            }
-        })
-    })
-}
-function mysqlQuerySpringMusic(con) {
-    return new Promise((resolve, reject) => {
-        con.query("SELECT * from album", (err, result, fields) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve({ rows: result, connection: con })
-            }
-        })
-    })
+let tests = [
+    { tag: 'mysql', testFunc: testMysql },
+    { tag: 'redis', testFunc: testRedis },
+]
+
+let credentials = vcapServices.findCredentials({ instance: { tags: tests[0].tag } });
+
+async function runTest(credentials, testFunc) {
+    try {
+        await testFunc(credentials, runServer)
+    } catch (e) {
+        console.error(e)
+    }
 }
 
-var credentials = vcapServices.findCredentials({ instance: { tags: 'mysql' } });
+for (test of tests) {
+    let credentials = vcapServices.findCredentials({ instance: { tags: test.tag } });
 
-var content=""
-if (credentials) {
-    connectMysql({
-        host: credentials.hostname,
-        user: credentials.username,
-        password: credentials.password,
-        port: credentials.port,
-        database: credentials.name
-    }).then((con) => {
-        return mysqlShowTables(con)
-    }).then((result) => {
-        content += JSON.stringify(result.tables)
-        return Promise.resolve(result.connection)
-    }).then((con) => {
-        return mysqlQuerySpringMusic(con)
-    }).then((result) => {
-        content += JSON.stringify(result.rows)
-        return Promise.resolve(result.connection)
-    }).then(() => {
-        return connectMysql(credentials.uri)
-    }).then((con) => {
-        return mysqlShowTables(con)
-    }).then((result) => {
-        content += JSON.stringify(result.tables)
-        return Promise.resolve(result.connection)
-    }).then((con) => {
-        return mysqlQuerySpringMusic(con)
-    }).then((result) => {
-        content += JSON.stringify(result.rows)
-        return Promise.resolve(result.connection)
-    }).then(() => {
-        var server = restify.createServer();
-        server.get('/', (_, res, next) => {
-            res.send(content)
-            next()
-        });
-
-        server.listen(process.env.PORT || 8080, function () {
-            console.log('%s listening at %s', server.name, server.url);
-        });
-    }).catch(error => {
-        console.error("mysql connection(s) failed", error)
-    })
-    
+    if (Object.keys(credentials).length > 0) {
+        console.log("testing %s", test.tag)
+        runTest(credentials, test.testFunc)
+    }
 }
+
+console.error('No services with tags matching any of:', tests.map((test) => { return test.tag }))
