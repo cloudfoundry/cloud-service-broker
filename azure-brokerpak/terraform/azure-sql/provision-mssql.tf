17a3a0d78dc2ce438708f6092c06fab91e98a5c2
Ernie Billing <ebilling@pivotal.io>
2020-03-18 16:59:26 -0700

    [#171751084] common lowercase plan names, admin password complexity

--- azure-brokerpak/terraform/azure-sql/provision-mssql.tf
+++ azure-brokerpak/terraform/azure-sql/provision-mssql.tf
@@ -13,8 +13,10 @@ resource "random_string" "username" {
 
 resource "random_password" "password" {
   length = 64
-  special = true
-  override_special = "_@#"
+  override_special = "~_-."
+  min_upper = 2
+  min_lower = 2
+  min_special = 2
 }
 
 resource "azurerm_resource_group" "azure_sql" {
