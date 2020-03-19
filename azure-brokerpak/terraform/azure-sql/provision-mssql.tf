cce651c9d57cbd18a51a80538d48f6c4174873e1
Ernie Billing <ebilling@pivotal.io>
2020-03-19 09:23:15 -0700

    [#171266712] sql failover group bind time credentials, added sql server credentials to acceptance test

--- azure-brokerpak/terraform/azure-sql/provision-mssql.tf
+++ azure-brokerpak/terraform/azure-sql/provision-mssql.tf
@@ -51,36 +51,16 @@ resource "azurerm_sql_firewall_rule" "example" {
   resource_group_name = azurerm_resource_group.azure_sql.name
   server_name         = azurerm_sql_server.azure_sql_db_server.name
   start_ip_address    = "0.0.0.0"
-  end_ip_address      = "255.255.255.255"
+  end_ip_address      = "0.0.0.0"
 }
 
-output "sqldbName" {value = "${azurerm_sql_database.azure_sql_db.name}"}
 output "sqldbResourceGroup" {value = "${azurerm_sql_server.azure_sql_db_server.resource_group_name}"}
+output "sqldbName" {value = "${azurerm_sql_database.azure_sql_db.name}"}
 output "sqlServerName" {value = "${azurerm_sql_server.azure_sql_db_server.name}"}
 output "sqlServerFullyQualifiedDomainName" {value = "${azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name}"}
-output "databaseLogin" {value = "${random_string.username.result}"}
-output "databaseLoginPassword" {value = "${random_password.password.result}"}
-output "jdbcUrl" {
-  value = format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;Encrypt=true;TrustServerCertificate=false;HostNameInCertificate=*.database.windows.net;loginTimeout=30", 
-                 azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name, 
-                 azurerm_sql_database.azure_sql_db.name, 
-                 random_string.username.result, 
-                 random_password.password.result)
-}
-output "jdbcUrlForAuditingEnabled" {
-  value = format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;Encrypt=true;TrustServerCertificate=false;HostNameInCertificate=*.database.windows.net;loginTimeout=30",            
-                 azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name, 
-                 azurerm_sql_database.azure_sql_db.name, 
-                 random_string.username.result, 
-                 random_password.password.result)
-}
 output "hostname" {value = "${azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name}"}
 output "port" {value = 1433}
 output "name" {value = "${azurerm_sql_database.azure_sql_db.name}"}
 output "username" {value = "${random_string.username.result}"}
 output "password" {value = "${random_password.password.result}"}
-output "uri" {
-  value = format("mssql://%s:1433/%s?encrypt=true&TrustServerCertificate=false&HostNameInCertificate=*.database.windows.net", 
-                 azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name, 
-                 azurerm_sql_database.azure_sql_db.name)
-}
+
