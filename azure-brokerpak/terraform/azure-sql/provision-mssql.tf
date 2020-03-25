9be653d7a0fa476857164a9d6034bc7dc99c7693
Ernie Billing <ebilling@pivotal.io>
2020-03-24 17:06:31 -0700

    Azure SQL and Azure Redis create resource group only if not specified. Otherwise use existing resource group.
    [#171961722]

--- azure-brokerpak/terraform/azure-sql/provision-mssql.tf
+++ azure-brokerpak/terraform/azure-sql/provision-mssql.tf
@@ -1,4 +1,4 @@
-variable server_name { type = string }
+variable instance_name { type = string }
 variable resource_group { type = string }
 variable db_name { type = string }
 variable region { type = string }
@@ -6,6 +6,17 @@ variable labels { type = map }
 variable pricing_tier { type = string }
 variable cores { type = number }
 variable storage_gb { type = number }
+
+locals {
+  resource_group = length(var.resource_group) == 0 ? format("rg-%s", var.instance_name) : var.resource_group
+}
+
+resource "azurerm_resource_group" "azure_sql" {
+  name     = local.resource_group
+  location = var.region
+  tags     = var.labels
+  count    = length(var.resource_group) == 0 ? 1 : 0
+}
 
 resource "random_string" "username" {
   length = 16
@@ -21,15 +32,10 @@ resource "random_password" "password" {
   min_special = 2
 }
 
-resource "azurerm_resource_group" "azure_sql" {
-  name     = var.resource_group
-  location = var.region
-  tags = var.labels
-}
-
 resource "azurerm_sql_server" "azure_sql_db_server" {
-  name                         = var.server_name
-  resource_group_name          = azurerm_resource_group.azure_sql.name
+  depends_on = [ azurerm_resource_group.azure_sql ]
+  name                         = var.instance_name
+  resource_group_name          = local.resource_group
   location                     = var.region
   version                      = "12.0"
   administrator_login          = random_string.username.result
@@ -49,19 +55,19 @@ resource "azurerm_sql_database" "azure_sql_db" {
 
 resource "azurerm_sql_firewall_rule" "example" {
   name                = "FirewallRule1"
-  resource_group_name = azurerm_resource_group.azure_sql.name
+  resource_group_name = local.resource_group
   server_name         = azurerm_sql_server.azure_sql_db_server.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "0.0.0.0"
 }
 
-output "sqldbResourceGroup" {value = "${azurerm_sql_server.azure_sql_db_server.resource_group_name}"}
-output "sqldbName" {value = "${azurerm_sql_database.azure_sql_db.name}"}
-output "sqlServerName" {value = "${azurerm_sql_server.azure_sql_db_server.name}"}
-output "sqlServerFullyQualifiedDomainName" {value = "${azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name}"}
-output "hostname" {value = "${azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name}"}
+output "sqldbResourceGroup" {value = azurerm_sql_server.azure_sql_db_server.resource_group_name}
+output "sqldbName" {value = azurerm_sql_database.azure_sql_db.name}
+output "sqlServerName" {value = azurerm_sql_server.azure_sql_db_server.name}
+output "sqlServerFullyQualifiedDomainName" {value = azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name}
+output "hostname" {value = azurerm_sql_server.azure_sql_db_server.fully_qualified_domain_name}
 output "port" {value = 1433}
-output "name" {value = "${azurerm_sql_database.azure_sql_db.name}"}
-output "username" {value = "${random_string.username.result}"}
-output "password" {value = "${random_password.password.result}"}
+output "name" {value = azurerm_sql_database.azure_sql_db.name}
+output "username" {value = random_string.username.result}
+output "password" {value = random_password.password.result}
 
