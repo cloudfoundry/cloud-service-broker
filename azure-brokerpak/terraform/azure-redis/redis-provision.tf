ce5ec874b479ad4d5b496b08b4736c496fa9e6ad
Ernie Billing <ebilling@pivotal.io>
2020-03-24 17:06:31 -0700

    Azure SQL and Azure Redis create resource group only if not specified. Otherwise use existing resource group.
    [#171961722]

--- azure-brokerpak/terraform/azure-redis/redis-provision.tf
+++ azure-brokerpak/terraform/azure-redis/redis-provision.tf
@@ -6,24 +6,30 @@ variable instance_name { type = string }
 variable region { type = string }
 variable labels { type = map }
 
+locals {
+  resource_group = length(var.resource_group) == 0 ? format("rg-%s", var.instance_name) : var.resource_group
+}
+
 resource "azurerm_resource_group" "azure-redis" {
-  name     = var.resource_group
+  name     = local.resource_group
   location = var.region
   tags     = var.labels
+  count    = length(var.resource_group) == 0 ? 1 : 0
 }
 
 resource "azurerm_redis_cache" "redis" {
+  depends_on = [ azurerm_resource_group.azure-redis ]  
   name                = var.instance_name
   sku_name            = var.sku_name
   family              = var.family
   capacity            = var.capacity
-  location            = azurerm_resource_group.azure-redis.location
-  resource_group_name = azurerm_resource_group.azure-redis.name
+  location            = var.region
+  resource_group_name = local.resource_group
   tags                = var.labels
 }
 
-output name { value = "${azurerm_redis_cache.redis.name}" }
-output hostname { value = "${azurerm_redis_cache.redis.hostname}" }
+output name { value = azurerm_redis_cache.redis.name }
+output hostname { value = azurerm_redis_cache.redis.hostname }
 output port { value = azurerm_redis_cache.redis.port }
-output password { value = "${azurerm_redis_cache.redis.primary_access_key}" }
-output tls_port { value = "${azurerm_redis_cache.redis.ssl_port}" }
+output password { value = azurerm_redis_cache.redis.primary_access_key }
+output tls_port { value = azurerm_redis_cache.redis.ssl_port }
