33cd34b29f3c3d9ed2a1098dab10e8f8fba80192
Ernie Billing <ebilling@pivotal.io>
2020-04-01 11:27:59 -0700

    moved azure secrets into team folder

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -4,10 +4,10 @@ set +x # Hide secrets
 
 [[ "${BASH_SOURCE[0]}" == "${0}" ]] && echo -e "You must source this script\nsource ${0}" && exit 1
 
-export ARM_SUBSCRIPTION_ID=$(lpass show --notes "Shared-CF Platform Engineering/Azure Service Account Key" | jq -r .subscription_id)
-export ARM_TENANT_ID=$(lpass show --notes "Shared-CF Platform Engineering/Azure Service Account Key" | jq -r .tenant_id)
-export ARM_CLIENT_ID=$(lpass show --notes "Shared-CF Platform Engineering/Azure Service Account Key" | jq -r .client_id)
-export ARM_CLIENT_SECRET=$(lpass show --notes "Shared-CF Platform Engineering/Azure Service Account Key" | jq -r .client_secret)
+export ARM_SUBSCRIPTION_ID=$(lpass show --notes "Shared-CF Platform Engineering/pe-cloud-service-broker/Azure Service Account Key" | jq -r .subscription_id)
+export ARM_TENANT_ID=$(lpass show --notes "Shared-CF Platform Engineering/pe-cloud-service-broker/Azure Service Account Key" | jq -r .tenant_id)
+export ARM_CLIENT_ID=$(lpass show --notes "Shared-CF Platform Engineering/pe-cloud-service-broker/Azure Service Account Key" | jq -r .client_id)
+export ARM_CLIENT_SECRET=$(lpass show --notes "Shared-CF Platform Engineering/pe-cloud-service-broker/Azure Service Account Key" | jq -r .client_secret)
 
 export GCP_SERVICE_ACCOUNT_JSON=$(lpass show --notes "Shared-CF Platform Engineering/pks cluster management gcp service account")
 export ROOT_SERVICE_ACCOUNT_JSON="${GCP_SERVICE_ACCOUNT_JSON}"
