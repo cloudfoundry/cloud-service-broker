6017012c35bd981af43f96be5fa7fc5319d29aae
Ernie Billing <ebilling@pivotal.io>
2020-04-06 09:27:07 -0700

    [#172068976] separate negative tests into separate test script

---
+++ acceptance-tests/azure/cf-test-negative-responses.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# bogus location or resource_group should fail
+../cf-create-service-should-fail.sh csb-azure-mysql medium '{"location":"bogus"}'
+../cf-create-service-should-fail.sh csb-azure-mysql medium '{"resource_group":"bogus"}'
+
+../cf-create-service-should-fail.sh csb-azure-redis medium '{"location":"bogus"}'
+../cf-create-service-should-fail.sh csb-azure-redis medium '{"resource_group":"bogus"}'
+
+../cf-create-service-should-fail.sh csb-azure-mongodb medium '{"location":"bogus"}'
+../cf-create-service-should-fail.sh csb-azure-mongodb medium '{"resource_group":"bogus"}'
+
+../cf-create-service-should-fail.sh csb-azure-mssql medium '{"location":"bogus"}'
+../cf-create-service-should-fail.sh csb-azure-mssql medium '{"resource_group":"bogus"}'
+
+../cf-create-service-should-fail.sh csb-azure-mssql-failover-group medium '{"location":"bogus"}'
+../cf-create-service-should-fail.sh csb-azure-mssql-failover-group medium '{"resource_group":"bogus"}'
+
+../cf-create-service-should-fail.sh csb-azure-eventhubs medium '{"location":"bogus"}'
+../cf-create-service-should-fail.sh csb-azure-eventhubs medium '{"resource_group":"bogus"}'
+
+../cf-create-service-should-fail.sh csb-azure-mssql-server standard '{"location":"bogus"}'
+../cf-create-service-should-fail.sh csb-azure-mssql-server standard '{"resource_group":"bogus"}'
--- acceptance-tests/azure/cf-test-services.sh
+++ acceptance-tests/azure/cf-test-services.sh
@@ -12,27 +12,6 @@ if [ $# -gt 1 ]; then
   SPRING_MUSIC_TEST=../cf-test-spring-music-credhub.sh
 fi
 
-# bogus location or resource_group should fail
-../cf-create-service-should-fail.sh csb-azure-mysql medium '{"location":"bogus"}'
-../cf-create-service-should-fail.sh csb-azure-mysql medium '{"resource_group":"bogus"}'
-
-../cf-create-service-should-fail.sh csb-azure-redis medium '{"location":"bogus"}'
-../cf-create-service-should-fail.sh csb-azure-redis medium '{"resource_group":"bogus"}'
-
-../cf-create-service-should-fail.sh csb-azure-mongodb medium '{"location":"bogus"}'
-../cf-create-service-should-fail.sh csb-azure-mongodb medium '{"resource_group":"bogus"}'
-
-../cf-create-service-should-fail.sh csb-azure-mssql medium '{"location":"bogus"}'
-../cf-create-service-should-fail.sh csb-azure-mssql medium '{"resource_group":"bogus"}'
-
-../cf-create-service-should-fail.sh csb-azure-mssql-failover-group medium '{"location":"bogus"}'
-../cf-create-service-should-fail.sh csb-azure-mssql-failover-group medium '{"resource_group":"bogus"}'
-
-../cf-create-service-should-fail.sh csb-azure-eventhubs medium '{"location":"bogus"}'
-../cf-create-service-should-fail.sh csb-azure-eventhubs medium '{"resource_group":"bogus"}'
-
-../cf-create-service-should-fail.sh csb-azure-mssql-server standard '{"location":"bogus"}'
-../cf-create-service-should-fail.sh csb-azure-mssql-server standard '{"resource_group":"bogus"}'
 
 # validate services against spring music 
 "${SPRING_MUSIC_TEST}" csb-azure-mysql small
