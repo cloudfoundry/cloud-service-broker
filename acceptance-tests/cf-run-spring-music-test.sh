8ad2ed74450b193413efb8ae14d862fef708133e
Ernie Billing <ebilling@pivotal.io>
2020-04-07 10:23:19 -0700

    removed error exit flag so app cleanup happens

--- acceptance-tests/cf-run-spring-music-test.sh
+++ acceptance-tests/cf-run-spring-music-test.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 
-set -o errexit
 set -o pipefail
 set -o nounset
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 . "${SCRIPT_DIR}/functions.sh"
-
 
 bind_service_test() {
   APP=$1
