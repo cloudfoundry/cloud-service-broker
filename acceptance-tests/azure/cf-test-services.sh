d15dcc6dd65c94e4f5a3ea03de43a92abfefc183
Ernie Billing <ebilling@pivotal.io>
2020-04-06 17:43:07 -0700

    [#172068976] refactored acceptance tests and properly enabled credhub tests

--- acceptance-tests/azure/cf-test-services.sh
+++ acceptance-tests/azure/cf-test-services.sh
@@ -9,12 +9,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 RESULT=1
 
-SPRING_MUSIC_TEST=../cf-test-spring-music.sh
-
-if [ $# -gt 1 ]; then
-  SPRING_MUSIC_TEST=../cf-test-spring-music-credhub.sh
-fi
-
 allServices=("csb-azure-mysql" "csb-azure-redis" "csb-azure-mssql" "csb-azure-mssql-failover-group")
 
 for s in ${allServices[@]}; do
@@ -28,6 +22,15 @@ allServices+=( "csb-azure-mongodb" )
 if wait; then
   RESULT=0
   for s in ${allServices[@]}; do
+    if [ $# -gt 0 ]; then
+      if "${SCRIPT_DIR}/../cf-validate-credhub.sh" "${s}-$$"; then
+        echo "SUCCEEDED: ${SCRIPT_DIR}/../cf-validate-credhub.sh ${s}-$$"
+      else
+        RESULT=1
+        echo "FAILED: ${SCRIPT_DIR}/../cf-validate-credhub.sh" "${s}-$$"
+        break
+      fi
+    fi
     if "${SCRIPT_DIR}/../cf-run-spring-music-test.sh" "${s}-$$"; then
       echo "SUCCEEDED: ${SCRIPT_DIR}/../cf-run-spring-music-test.sh" "${s}-$$"
     else
@@ -42,9 +45,10 @@ for s in ${allServices[@]}; do
   delete_service "${s}-$$" &
 done
 
-./cf-test-mssql-db.sh
-./cf-test-mssql-db-fog.sh
-./cf-test-mssql-do-failover.sh
+if [ ${RESULT} -eq 0 ]; then
+  ./cf-test-mssql-db.sh && ./cf-test-mssql-db-fog.sh && ./cf-test-mssql-do-failover.sh
+  RESULT=$?
+fi
 
 wait
 
