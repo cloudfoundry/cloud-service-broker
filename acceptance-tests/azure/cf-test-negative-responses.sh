a4c03c448091c41451fe9c312603a39de7805d74
Ernie Billing <ebilling@pivotal.io>
2020-04-22 11:42:32 -0700

    working on service cleanup, bogus subscription id leaves CC catalog in bad state

--- acceptance-tests/azure/cf-test-negative-responses.sh
+++ acceptance-tests/azure/cf-test-negative-responses.sh
@@ -7,7 +7,7 @@ set -o nounset
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # bogus location or resource_group should fail
-ALL_SERVICES=("csb-azure-mysql" "csb-azure-redis" "csb-azure-mongodb" "csb-azure-mssql" "csb-azure-mssql-failover-group" "csb-azure-mssql-server" "csb-azure-postgresql")
+ALL_SERVICES=("csb-azure-mysql" "csb-azure-redis" "csb-azure-mongodb" "csb-azure-mssql" "csb-azure-mssql-failover-group" "csb-azure-postgresql")
 
 BOGUS_OPTIONS=('{"location":"bogus"}' '{"resource_group":"bogus"}' '{"azure_subscription_id":"bogus"}')
 
@@ -15,6 +15,10 @@ for s in ${ALL_SERVICES[@]}; do
     for o in ${BOGUS_OPTIONS[@]}; do
         ${SCRIPT_DIR}/../cf-create-service-should-fail.sh "${s}" medium ${o}
     done
+done
+
+for o in ${BOGUS_OPTIONS[@]}; do
+    ${SCRIPT_DIR}/../cf-create-service-should-fail.sh csb-azure-mssql-server standard ${o}
 done
 
 for o in ${BOGUS_OPTIONS[@]}; do
