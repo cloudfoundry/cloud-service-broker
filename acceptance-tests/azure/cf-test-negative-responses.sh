62393eff0b7ac6fca93d10e5d481f7df2e60ca24
Ernie Billing <ebilling@pivotal.io>
2020-04-22 09:34:58 -0700

    acceptance test service cleanup retries

--- acceptance-tests/azure/cf-test-negative-responses.sh
+++ acceptance-tests/azure/cf-test-negative-responses.sh
@@ -7,12 +7,18 @@ set -o nounset
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # bogus location or resource_group should fail
-ALL_SERVICES=("csb-azure-mysql" "csb-azure-redis" "csb-azure-mongodb" "csb-azure-mssql" "csb-azure-mssql-failover-group" "csb-azure-eventhubs" "csb-azure-mssql-server" "csb-azure-postgresql")
+ALL_SERVICES=("csb-azure-mysql" "csb-azure-redis" "csb-azure-mongodb" "csb-azure-mssql" "csb-azure-mssql-failover-group" "csb-azure-mssql-server" "csb-azure-postgresql")
+
+BOGUS_OPTIONS=('{"location":"bogus"}' '{"resource_group":"bogus"}' '{"azure_subscription_id":"bogus"}')
 
 for s in ${ALL_SERVICES[@]}; do
-    ${SCRIPT_DIR}/../cf-create-service-should-fail.sh "${s}" medium '{"location":"bogus"}'
-    ${SCRIPT_DIR}/../cf-create-service-should-fail.sh "${s}" medium '{"resource_group":"bogus"}'
-    ${SCRIPT_DIR}/../cf-create-service-should-fail.sh "${s}" medium '{"azure_subscription_id":"bogus"}'
+    for o in ${BOGUS_OPTIONS[@]}; do
+        ${SCRIPT_DIR}/../cf-create-service-should-fail.sh "${s}" medium ${o}
+    done
+done
+
+for o in ${BOGUS_OPTIONS[@]}; do
+    ${SCRIPT_DIR}/../cf-create-service-should-fail.sh csb-azure-eventhubs basic ${o}
 done
 
 ${SCRIPT_DIR}/../cf-create-service-should-fail.sh csb-azure-postgresql medium '{"sku_name":"bogus"}'
