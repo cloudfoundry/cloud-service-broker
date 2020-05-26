919a1bf68437d4a19fe259dd0ca128c66f651cbd
Sridhar Vennela <svennela@pivotal.io>
2020-05-26 16:17:14 -0700

    updated google depencey provider to fix bind template for bigquery

--- gcp-brokerpak/terraform/bind-bigquery.tf
+++ gcp-brokerpak/terraform/bind-bigquery.tf
@@ -1,14 +1,11 @@
 variable dataset_id { type = string }
 variable role { type = string }
-variable service_account_name {type = "string"}
-variable service_account_display_name {type = "string"}
+variable service_account_name {type = string }
+variable service_account_display_name {type = string}
 variable project  { type = string }
 variable credentials  { type = string }
 
-provider "google-beta" {
-  version = ">=3.22.0"
-  project     = var.project
-}
+
   
   
 resource "google_service_account" "account" {
