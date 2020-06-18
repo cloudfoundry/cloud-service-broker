7ea39835c8159618fb30d42f5358b5ed5a3bf979
Ernie Billing <ebilling@pivotal.io>
2020-06-18 15:31:58 -0700

    Removed service network connection - Alana will have to pre-configure.
    [#172424919]
    
    Co-authored-by: Sridhar Vennela <svennela@pivotal.io>

--- gcp-brokerpak/terraform/cloud-sql-provision.tf
+++ gcp-brokerpak/terraform/cloud-sql-provision.tf
@@ -36,19 +36,19 @@ data "google_compute_network" "authorized-network" {
   name = var.authorized_network
 }
 
-resource "google_compute_global_address" "private_ip_address" {
-  name          = "priv-ip-addr-${var.instance_name}"
-  purpose       = "VPC_PEERING"
-  address_type  = "INTERNAL"
-  prefix_length = 24
-  network       = data.google_compute_network.authorized-network.self_link
-}
+# resource "google_compute_global_address" "private_ip_address" {
+#   name          = "priv-ip-addr-${var.instance_name}"
+#   purpose       = "VPC_PEERING"
+#   address_type  = "INTERNAL"
+#   prefix_length = 24
+#   network       = data.google_compute_network.authorized-network.self_link
+# }
 
-resource "google_service_networking_connection" "private_vpc_connection" {
-  network                 = data.google_compute_network.authorized-network.self_link
-  service                 = "servicenetworking.googleapis.com"
-  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-}
+# resource "google_service_networking_connection" "private_vpc_connection" {
+#   network                 = data.google_compute_network.authorized-network.self_link
+#   service                 = "servicenetworking.googleapis.com"
+#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+# }
 
 locals {
   service_tiers = {
@@ -71,7 +71,7 @@ resource "google_sql_database_instance" "instance" {
   database_version = var.database_version
   region           = var.region
 
-  depends_on = [google_service_networking_connection.private_vpc_connection]
+  # depends_on = [google_service_networking_connection.private_vpc_connection]
 
   settings {
     tier = local.service_tiers[var.cores]
