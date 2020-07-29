56e88f9f19062ae2da2d7d9f635d90a05ec19ea4
Sridhar Vennela <svennela@pivotal.io>
2020-07-29 08:58:02 -0700

    added base64 format for Credentials in output

--- gcp-brokerpak/terraform/bind-dataproc.tf
+++ gcp-brokerpak/terraform/bind-dataproc.tf
@@ -26,3 +26,4 @@
     output "email" {value = "${google_service_account.account.email}"}
     output "private_key" {value = "${google_service_account_key.key.private_key}"}
     output "project_id" {value = "${google_service_account.account.project}"}
+    output "Credentials" { value = base64decode(google_service_account_key.key.private_key) }
