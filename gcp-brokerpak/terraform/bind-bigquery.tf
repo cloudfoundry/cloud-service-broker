0b39c4d6de903faf8c8eda9210ba3652cd78bc94
sridhar vennela <sridhar.vennela@gmail.com>
2020-09-01 09:11:49 -0700

    Test app gcp spanner (#74)
    * added spring boot app for gcp storage service
    
    * added spring boot app for gcp storage service
    
    * deleted gcp token
    
    * added base64 format for Credentials in output
    
    Co-authored-by: Sridhar Vennela <svennela@pivotal.io>

--- gcp-brokerpak/terraform/bind-bigquery.tf
+++ gcp-brokerpak/terraform/bind-bigquery.tf
@@ -56,5 +56,4 @@ output "UniqueId" {value = "${google_service_account.account.unique_id}"}
 output "PrivateKeyData" {value = "${google_service_account_key.key.private_key}"}
 output "ProjectId" {value = "${google_service_account.account.project}"}
 output dataset_id { value = var.dataset_id }
-
-
+output "Credentials" { value = base64decode(google_service_account_key.key.private_key) }
