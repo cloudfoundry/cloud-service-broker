eb50b27d7c4abb8669eecd7d5b4d62b40584d091
Sridhar Vennela <svennela@pivotal.io>
2020-09-03 12:41:03 -0700

    create subnet fix for gcp

--- gcp-brokerpak/terraform/cloud-sql-provision.tf
+++ gcp-brokerpak/terraform/cloud-sql-provision.tf
@@ -36,19 +36,19 @@ data "google_compute_network" "authorized-network" {
   name = var.authorized_network
 }
 
-# resource "google_compute_global_address" "private_ip_address" {
-#   name          = "priv-ip-addr-${var.instance_name}"
-#   purpose       = "VPC_PEERING"
-#   address_type  = "INTERNAL"
-#   prefix_length = 24
-#   network       = data.google_compute_network.authorized-network.self_link
-# }
+resource "google_compute_global_address" "private_ip_address" {
+  name          = "priv-ip-addr-${var.instance_name}"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 24
+  network       = data.google_compute_network.authorized-network.self_link
+}
 
-# resource "google_service_networking_connection" "private_vpc_connection" {
-#   network                 = data.google_compute_network.authorized-network.self_link
-#   service                 = "servicenetworking.googleapis.com"
-#   reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
-# }
+resource "google_service_networking_connection" "private_vpc_connection" {
+  network                 = data.google_compute_network.authorized-network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
 
 locals {
   service_tiers = {
