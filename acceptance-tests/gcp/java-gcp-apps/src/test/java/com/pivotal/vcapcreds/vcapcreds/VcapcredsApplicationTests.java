52d9aba7d76507991ce7ee48a559a43e0e19ffaa
sridhar vennela <sridhar.vennela@gmail.com>
2020-09-14 17:13:05 -0700

    added spring-boot app for gcpbigquery,gcpspanner,gcpbucket (#88)
    * added spring-boot app for gcpbigquery,gcpspanner,gcpbucket
    
    * Update buildandpush.sh
    
    Co-authored-by: Sridhar Vennela <svennela@pivotal.io>

---
+++ acceptance-tests/gcp/java-gcp-apps/src/test/java/com/pivotal/vcapcreds/vcapcreds/VcapcredsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pivotal.vcapcreds.vcapcreds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VcapcredsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
