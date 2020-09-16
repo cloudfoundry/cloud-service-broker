d873ce97e5c172ae87f86210195dd992abd8cc32
Sridhar Vennela <svennela@pivotal.io>
2020-09-16 13:07:44 -0700

    aws postgres import script

--- acceptance-tests/aws/cf-test-services.sh
+++ acceptance-tests/aws/cf-test-services.sh
@@ -43,7 +43,7 @@ for s in ${allServices[@]}; do
 done
 
 if [ ${RESULT} -eq 0 ]; then
-  ${SCRIPT_DIR}/cf-test-s3-bucket.sh 
+  ${SCRIPT_DIR}/cf-test-s3-bucket.sh && ${SCRIPT_DIR}/cf-test-postgres-sql-subsume.sh "${AWS-LEGACY-POSTGRES-DBINSTANCE}" "${AWS-LEGACY-POSTGRES-REGION}" "${AWS-LEGACY-POSTGRES-ADMINPASSWORD}" 
   RESULT=$?
 fi
 
