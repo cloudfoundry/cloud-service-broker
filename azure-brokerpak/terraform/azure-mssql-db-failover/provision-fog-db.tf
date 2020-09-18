5280a6994ac86499e4a299f1db8b2e2a524413c8
Ernie Billing <ebilling@pivotal.io>
2020-09-18 15:07:00 -0700

    [#173818764,#174156158] cf service ... output azure resource ids for dbs so they can be subsumed easily

--- azure-brokerpak/terraform/azure-mssql-db-failover/provision-fog-db.tf
+++ azure-brokerpak/terraform/azure-mssql-db-failover/provision-fog-db.tf
@@ -103,4 +103,17 @@ output port {value = 1433}
 output name {value = var.db_name}
 output username {value = var.server_credential_pairs[var.server_pair].admin_username}
 output password {value = var.server_credential_pairs[var.server_pair].admin_password}
-output status {value=format("fog id: %s", azurerm_sql_failover_group.failover_group.id)}
+output status {
+    value = var.existing ? format("connected to existing failover group - primary server %s (id: %s) secondary server %s (%s) URL: https://portal.azure.com/#@%s/resource%s/failoverGroup",
+                                              data.azurerm_sql_server.primary_sql_db_server.name, data.azurerm_sql_server.primary_sql_db_server.id,
+                                              data.azurerm_sql_server.secondary_sql_db_server.name, data.azurerm_sql_server.secondary_sql_db_server.id,
+                                              var.azure_tenant_id,
+                                              data.azurerm_sql_server.primary_sql_db_server.id) : format("created failover group %s (id: %s), primary db %s (id: %s) on server %s (id: %s), secondary db %s (id: %s/databases/%s) on server %s (id: %s) URL: https://portal.azure.com/#@%s/resource%s/failoverGroup",
+                                              azurerm_sql_failover_group.failover_group[0].name, azurerm_sql_failover_group.failover_group[0].id,
+                                              azurerm_sql_database.azure_sql_db_primary[0].name, azurerm_sql_database.azure_sql_db_primary[0].id,
+                                              data.azurerm_sql_server.primary_sql_db_server.name, data.azurerm_sql_server.primary_sql_db_server.id,
+                                              azurerm_sql_database.azure_sql_db_primary[0].name, data.azurerm_sql_server.secondary_sql_db_server.id, azurerm_sql_database.azure_sql_db_primary[0].name,
+                                              data.azurerm_sql_server.secondary_sql_db_server.name, data.azurerm_sql_server.secondary_sql_db_server.id,
+                                              var.azure_tenant_id,
+                                              data.azurerm_sql_server.primary_sql_db_server.id)
+}
--- azure-brokerpak/terraform/azure-mssql-failover/provision-mssql-failover.tf
+++ azure-brokerpak/terraform/azure-mssql-failover/provision-mssql-failover.tf
@@ -207,11 +207,19 @@ resource "azurerm_sql_firewall_rule" "server2" {
 locals {
     serverFQDN = format("%s.database.windows.net", azurerm_sql_failover_group.failover_group.name)
 }
-output "sqldbName" {value = azurerm_sql_database.azure_sql_db.name}
-output "sqlServerName" {value = azurerm_sql_failover_group.failover_group.name}
-output "sqlServerFullyQualifiedDomainName" {value = local.serverFQDN}
-output "hostname" {value = local.serverFQDN}
-output "port" {value = 1433}
-output "name" {value = azurerm_sql_database.azure_sql_db.name}
-output "username" {value = random_string.username.result}
-output "password" {value = random_password.password.result}
+output sqldbName {value = azurerm_sql_database.azure_sql_db.name}
+output sqlServerName {value = azurerm_sql_failover_group.failover_group.name}
+output sqlServerFullyQualifiedDomainName {value = local.serverFQDN}
+output hostname {value = local.serverFQDN}
+output port {value = 1433}
+output name {value = azurerm_sql_database.azure_sql_db.name}
+output username {value = random_string.username.result}
+output password {value = random_password.password.result}
+output status {value = format("created failover group %s (id: %s), primary db %s (id: %s) on server %s (id: %s), secondary db %s (id: %s/%s) on server %s (id: %s) URL: https://portal.azure.com/#@%s/resource%s/failoverGroup",
+                              azurerm_sql_failover_group.failover_group.name, azurerm_sql_failover_group.failover_group.id,
+                              azurerm_sql_database.azure_sql_db.name, azurerm_sql_database.azure_sql_db.id,
+                              azurerm_sql_server.primary_azure_sql_db_server.name, azurerm_sql_server.primary_azure_sql_db_server.id,
+                              azurerm_sql_database.azure_sql_db.name, azurerm_sql_server.secondary_sql_db_server.id, azurerm_sql_database.azure_sql_db.name,
+                              azurerm_sql_server.secondary_sql_db_server.name, azurerm_sql_server.secondary_sql_db_server.id,
+                              var.azure_tenant_id,
+                              azurerm_sql_server.primary_azure_sql_db_server.id)}
