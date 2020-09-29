75f760f4cb32bf2c6282a7ac8b31845191fc2a6f
Ernie Billing <ebilling@pivotal.io>
2020-09-29 13:44:54 -0700

    [#174975859] added resource info to status for azure services

--- azure-brokerpak/terraform/azure-mongodb/azure-mongodb.tf
+++ azure-brokerpak/terraform/azure-mongodb/azure-mongodb.tf
@@ -103,3 +103,8 @@ resource "azurerm_cosmosdb_mongo_collection" "mongo-collection" {
 }
 
 output uri { value = replace(azurerm_cosmosdb_account.mongo-account.connection_strings[0], "/?", "/${azurerm_cosmosdb_mongo_database.mongo-db.name}?")  }
+output status { value = format("created db %s (id: %s) URL:  https://portal.azure.com/#@%s/resource%s",
+                               azurerm_cosmosdb_mongo_database.mongo-db.name,
+                               azurerm_cosmosdb_mongo_database.mongo-db.id,
+                               var.azure_tenant_id,
+                               azurerm_cosmosdb_mongo_database.mongo-db.id)}
--- azure-brokerpak/terraform/azure-storage/account-provision.tf
+++ azure-brokerpak/terraform/azure-storage/account-provision.tf
@@ -80,3 +80,8 @@ resource "azurerm_storage_account_network_rules" "account_network_rule" {
 output primary_access_key { value = azurerm_storage_account.account.primary_access_key }
 output secondary_access_key { value = azurerm_storage_account.account.secondary_access_key }
 output storage_account_name { value = azurerm_storage_account.account.name }
+output status { value = format("created storage account %s (id: %s) URL:  https://portal.azure.com/#@%s/resource%s",
+                               azurerm_storage_account.account.name,
+                               azurerm_storage_account.account.id,
+                               var.azure_tenant_id,
+                               azurerm_storage_account.account.id)}
