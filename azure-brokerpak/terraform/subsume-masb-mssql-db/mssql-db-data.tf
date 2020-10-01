af37cfad58435985b39d64af9b1fb85e425c5339
sridhar vennela <sridhar.vennela@gmail.com>
2020-10-01 14:52:43 -0700

    added server creds to mssql subsume service (#106)
    Co-authored-by: Sridhar Vennela <svennela@pivotal.io>

--- azure-brokerpak/terraform/subsume-masb-mssql-db/mssql-db-data.tf
+++ azure-brokerpak/terraform/subsume-masb-mssql-db/mssql-db-data.tf
@@ -1,10 +1,5 @@
 
-# data "azurerm_sql_server" "azure_sql_db_server" {
-#   name                         = var.server_name
-#   resource_group_name          = var.server_resource_group
-# }
-
 data "azurerm_sql_server" "azure_sql_db_server" {
-  name                         = azurerm_sql_database.azure_sql_db.server_name
-  resource_group_name          = azurerm_sql_database.azure_sql_db.resource_group_name
+  name                         = var.server_credentials[var.server].server_name
+  resource_group_name          = var.server_credentials[var.server].server_resource_group
 }
