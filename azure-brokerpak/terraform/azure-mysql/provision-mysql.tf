244f667a9c19c269482ff5fb55cfcf63e315e91f
Michael Claassen <michael_claassen@manulife.com>
2020-10-14 14:35:03 -0400

    Update AzureRM provider to version 2.31.0. Add support for Azure SQL short term retention period. (#112)

--- azure-brokerpak/terraform/azure-mysql/provision-mysql.tf
+++ azure-brokerpak/terraform/azure-mysql/provision-mysql.tf
@@ -31,7 +31,7 @@ variable tls_min_version { type = string }
 variable skip_provider_registration { type = bool }
 
 provider "azurerm" {
-  version = "~> 2.20.0"
+  version = "~> 2.31.0"
   features {}
 
   subscription_id = var.azure_subscription_id
