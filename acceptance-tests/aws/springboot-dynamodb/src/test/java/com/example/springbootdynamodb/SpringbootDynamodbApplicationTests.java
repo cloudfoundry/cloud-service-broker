bc67001e5376b91ca6ae0c1338e24d1b9b1aeed7
Sridhar Vennela <sridhar.vennela@gmail.com>
2020-10-28 10:08:48 -0700

    added dynamodb service

---
+++ acceptance-tests/aws/springboot-dynamodb/src/test/java/com/example/springbootdynamodb/SpringbootDynamodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdynamodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDynamodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
