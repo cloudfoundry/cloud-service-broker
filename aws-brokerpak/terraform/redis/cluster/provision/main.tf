b2a6799597ecc9f82bbe01e2bc3e68d9fdbd8659
Ernie Billing <ebilling@pivotal.io>
2020-12-07 09:38:02 -0800

    175945487 redis subnet (#150)
    * [#175945487] WIP: added parameters
    
    * [#175945487] security_group_id array fix
    
    * [#175945487] docs for redis subnet groups

--- aws-brokerpak/terraform/redis/cluster/provision/main.tf
+++ aws-brokerpak/terraform/redis/cluster/provision/main.tf
@@ -12,6 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+resource "aws_security_group" "sg" {
+  count = length(var.vpc_security_group_ids) == 0 ? 1 : 0    
+  name   = format("%s-sg", var.instance_name)
+  vpc_id = data.aws_vpc.vpc.id
+}
+
+resource "aws_elasticache_subnet_group" "subnet_group" {
+  count = length(var.elasticache_subnet_group) == 0 ? 1 : 0    
+  name = format("%s-p-sn", var.instance_name)
+  subnet_ids = data.aws_subnet_ids.all.ids
+}
+
+resource "aws_security_group_rule" "inbound_access" {
+  count = length(var.vpc_security_group_ids) == 0 ? 1 : 0    
+  from_port         = local.port
+  protocol          = "tcp"
+  security_group_id = aws_security_group.sg[0].id
+  to_port           = local.port
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "random_password" "auth_token" {
+  length = 64
+  // https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/auth.html
+  override_special = "!&#$^<>-"
+  min_upper = 2
+  min_lower = 2
+  min_special = 2
+}
+
 resource "aws_elasticache_replication_group" "redis" {
   automatic_failover_enabled    = var.node_count > 1
   replication_group_id          = var.instance_name
@@ -21,8 +52,8 @@ resource "aws_elasticache_replication_group" "redis" {
   parameter_group_name          = local.parameter_group_names[var.redis_version]
   port                          = local.port
   tags                          = var.labels
-  security_group_ids            = [aws_security_group.sg.id]
-  subnet_group_name             = aws_elasticache_subnet_group.subnet_group.name
+  security_group_ids            = local.vpc_security_group_ids
+  subnet_group_name             = local.subnet_group
   transit_encryption_enabled    = true
   auth_token                    = random_password.auth_token.result
 }
