eded23f5e73090381263f23fb825be865150c63a
George Blue <gblue@pivotal.io>
2021-02-26 21:59:35 +0000

    chore: add "staticcheck" static analysis
    Some rules were disabled because the fail with the code as it currently
    is. The aim is to fix up the code and remove the exclusions.

--- tools/tools.go
+++ tools/tools.go
@@ -4,6 +4,7 @@ package tools
 
 import (
 	_ "golang.org/x/tools/cmd/goimports"
+	_ "honnef.co/go/tools/cmd/staticcheck"
 )
 
 // This file imports packages that are used when running go generate, or used
