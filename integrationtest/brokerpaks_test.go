5c3f1b374c6f5f11be4832eb936b27f3742ae54f
George Blue <gblue@pivotal.io>
2021-05-24 16:24:53 +0100

    fix: terraform_resources.source should not be required
    The `source` URL in the terraform_resources section of the manifest
    doesn't make sense when the resource is actually a local file. This has
    resulted in an anti-pattern in the Azure brokerpak where a URL to an
    unused small file was added - it appears just to satisfy this
    requirement. From now on, `source` should be optional.
    
    [#178265962](https://www.pivotaltracker.com/story/show/178265962)

--- integrationtest/brokerpaks_test.go
+++ integrationtest/brokerpaks_test.go
@@ -6,6 +6,7 @@ import (
 	"path"
 	"time"
 
+	"github.com/cloudfoundry-incubator/cloud-service-broker/internal/zippy"
 	. "github.com/onsi/gomega/gbytes"
 	. "github.com/onsi/gomega/gexec"
 
@@ -49,6 +50,48 @@ var _ = Describe("Brokerpaks", func() {
 
 			Expect(session.ExitCode()).NotTo(BeZero())
 			Expect(session.Err).To(Say("duplicated value, must be unique: 8b52a460-b246-11eb-a8f5-d349948e2480: services\\[1\\].plans\\[1\\].Id\n"))
+		})
+	})
+
+	Describe("file inclusion", func() {
+		BeforeEach(func() {
+			err := os.WriteFile(path.Join(workDir, "extrafile.sh"), []byte("echo hello"), 0777)
+			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("includes the files", func() {
+			brokerpakPath := path.Join(workDir, "fake-brokerpak-0.1.0.brokerpak")
+			By("building the brokerpak", func() {
+				command := exec.Command(csb, "pak", "build", path.Join(fixturesDir, "brokerpak-file-inclusion"))
+				session, err := Start(command, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				session.Wait(time.Minute)
+
+				Expect(session.ExitCode()).To(BeZero())
+
+				Expect(brokerpakPath).To(BeAnExistingFile())
+			})
+
+			extractedPath := path.Join(workDir, "extracted")
+			By("unzipping the brokerpak", func() {
+				zr, err := zippy.Open(brokerpakPath)
+				Expect(err).NotTo(HaveOccurred())
+
+				err = zr.Extract("", extractedPath)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			By("checking that the expected files are there", func() {
+				paths := []string{
+					"bin/linux/amd64/terraform",
+					"bin/linux/amd64/cloud-service-broker.linux",
+					"bin/linux/amd64/extrafile.sh",
+					"src/terraform.zip",
+				}
+				for _, p := range paths {
+					Expect(path.Join(extractedPath, p)).To(BeAnExistingFile())
+				}
+			})
 		})
 	})
 })
