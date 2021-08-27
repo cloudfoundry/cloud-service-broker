33af9530f4e5c52fc63563c7a24ba4d7cc487b70
Felisia Martini <fmartini@pivotal.io>
2021-08-27 16:48:10 +0100

    Added integration test for restart scenario

--- integrationtest/database_encryption_test.go
+++ integrationtest/database_encryption_test.go
@@ -101,6 +101,15 @@ var _ = Describe("Database Encryption", func() {
 		return record.Label
 	}
 
+	expectOnlyOneEncryptionConfig := func() {
+		db, err := gorm.Open("sqlite3", databaseFile)
+		Expect(err).NotTo(HaveOccurred())
+		defer db.Close()
+		count := 0
+		err = db.Model(models.EncryptionDetail{}).Count(&count).Error
+		Expect(count).To(Equal(1))
+	}
+
 	expectServiceBindingDetailsToNotExist := func() {
 		db, err := gorm.Open("sqlite3", databaseFile)
 		Expect(err).NotTo(HaveOccurred())
@@ -273,6 +282,8 @@ var _ = Describe("Database Encryption", func() {
 	})
 
 	When("the encryption key is configured", func() {
+		var newBrokerSession *Session
+
 		BeforeEach(func() {
 			encryptionLabel = "first-key"
 			encryptionKeys := fmt.Sprintf("[{\"encryption_key\": {\"secret\":\"one-very-long-password-here\"},\"guid\":\"dae1dd13-53ed-4c90-8c11-7383b767d5c3\",\"label\":\"%s\",\"primary\":true\n}]", encryptionLabel)
@@ -280,6 +291,10 @@ var _ = Describe("Database Encryption", func() {
 				"CSB_ENABLE_ENCRYPTION=true",
 				fmt.Sprintf("CSB_ENCRYPTION_KEYS=%s", encryptionKeys),
 			}
+		})
+
+		AfterEach(func() {
+			newBrokerSession.Terminate()
 		})
 
 		It("encrypts sensitive fields", func() {
@@ -330,6 +345,56 @@ var _ = Describe("Database Encryption", func() {
 					ContainSubstring(tfStateKey),
 				))
 			})
+		})
+
+		It("uses the same key on restart", func() {
+
+			By("checking the encryption key config is stored", func() {
+				Expect(persistedEncryptionConfig()).To(Equal(encryptionLabel))
+			})
+
+			By("terminating old broker session")
+			brokerSession.Terminate()
+
+			By("starting the broker again", func() {
+				runBrokerCommand := exec.Command(csb, "serve")
+				os.Unsetenv("CH_CRED_HUB_URL")
+
+				runBrokerCommand.Env = append(
+					os.Environ(),
+					"CSB_LISTENER_HOST=localhost",
+					"DB_TYPE=sqlite3",
+					fmt.Sprintf("DB_PATH=%s", databaseFile),
+					fmt.Sprintf("PORT=%d", brokerPort),
+					fmt.Sprintf("SECURITY_USER_NAME=%s", brokerUsername),
+					fmt.Sprintf("SECURITY_USER_PASSWORD=%s", brokerPassword),
+				)
+				runBrokerCommand.Env = append(
+					runBrokerCommand.Env,
+					encryptionConfig...,
+				)
+				var err error
+				newBrokerSession, err = Start(runBrokerCommand, GinkgoWriter, GinkgoWriter)
+				Expect(err).NotTo(HaveOccurred())
+				waitForBrokerToStart(brokerPort)
+
+				brokerClient, err = client.New(brokerUsername, brokerPassword, "localhost", brokerPort)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			By("checking only one encryption detail record is set", func() {
+				expectOnlyOneEncryptionConfig()
+			})
+
+			By("checking how update persists service instance fields")
+			updateServiceInstance()
+			Expect(persistedRequestDetails()).NotTo(Equal(provisionParams))
+			Expect(persistedServiceInstanceDetails()).NotTo(Equal(updateOutput))
+			Expect(persistedServiceInstanceTerraformWorkspace()).NotTo(SatisfyAny(
+				ContainSubstring(provisionOutputStateValue),
+				ContainSubstring(updateOutputStateValue),
+				ContainSubstring(tfStateKey),
+			))
 		})
 	})
 })
