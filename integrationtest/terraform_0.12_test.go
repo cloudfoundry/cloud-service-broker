68774dd5f61f630065bc84580a7854a8f6915812
George Blue <gblue@pivotal.io>
2021-10-18 14:25:38 +0100

    test: Terraform 0.12
    As we look to support other Terraform versions, let's add a test to make
    sure that Terraform 0.12 continues to work
    
    [#179924320](https://www.pivotaltracker.com/story/show/179924320)
    
    Co-authored-by: James Norman <normanja@vmware.com>

---
+++ integrationtest/terraform_0.12_test.go
@@ -0,0 +1,103 @@
+package integrationtest_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"os/exec"
+	"path"
+	"time"
+
+	"github.com/pivotal-cf/brokerapi/v8/domain"
+
+	"github.com/cloudfoundry-incubator/cloud-service-broker/pkg/client"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+	"github.com/pborman/uuid"
+	_ "gorm.io/driver/sqlite"
+)
+
+var _ = Describe("Terraform 0.12", func() {
+	var (
+		originalDir         string
+		fixturesDir         string
+		workDir             string
+		brokerPort          int
+		brokerUsername      string
+		brokerPassword      string
+		brokerSession       *Session
+		brokerClient        *client.Client
+		databaseFile        string
+		serviceInstanceGUID string
+	)
+
+	JustBeforeEach(func() {
+		var err error
+		originalDir, err = os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		fixturesDir = path.Join(originalDir, "fixtures", "brokerpak-terraform-0.12")
+
+		workDir, err = os.MkdirTemp("", "*-csb-test")
+		Expect(err).NotTo(HaveOccurred())
+		err = os.Chdir(workDir)
+		Expect(err).NotTo(HaveOccurred())
+
+		buildBrokerpakCommand := exec.Command(csb, "pak", "build", fixturesDir)
+		session, err := Start(buildBrokerpakCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session, 10*time.Minute).Should(Exit(0))
+
+		brokerUsername = uuid.New()
+		brokerPassword = uuid.New()
+		brokerPort = freePort()
+		databaseFile = path.Join(workDir, "databaseFile.dat")
+		runBrokerCommand := exec.Command(csb, "serve")
+		runBrokerCommand.Env = append(
+			os.Environ(),
+			"CSB_LISTENER_HOST=localhost",
+			"DB_TYPE=sqlite3",
+			fmt.Sprintf("DB_PATH=%s", databaseFile),
+			fmt.Sprintf("PORT=%d", brokerPort),
+			fmt.Sprintf("SECURITY_USER_NAME=%s", brokerUsername),
+			fmt.Sprintf("SECURITY_USER_PASSWORD=%s", brokerPassword),
+		)
+		brokerSession, err = Start(runBrokerCommand, GinkgoWriter, GinkgoWriter)
+		Expect(err).NotTo(HaveOccurred())
+		waitForBrokerToStart(brokerPort)
+
+		brokerClient, err = client.New(brokerUsername, brokerPassword, "localhost", brokerPort)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		brokerSession.Terminate()
+
+		err := os.Chdir(originalDir)
+		Expect(err).NotTo(HaveOccurred())
+
+		err = os.RemoveAll(workDir)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("can provision using this Terraform version", func() {
+		const serviceOfferingGUID = "df2c1512-3013-11ec-8704-2fbfa9c8a802"
+		const servicePlanGUID = "e59773ce-3013-11ec-9bbb-9376b4f72d14"
+		serviceInstanceGUID = uuid.New()
+		provisionResponse := brokerClient.Provision(serviceInstanceGUID, serviceOfferingGUID, servicePlanGUID, uuid.New(), nil)
+		Expect(provisionResponse.Error).NotTo(HaveOccurred())
+		Expect(provisionResponse.StatusCode).To(Equal(http.StatusAccepted))
+
+		Eventually(func() bool {
+			lastOperationResponse := brokerClient.LastOperation(serviceInstanceGUID, requestID())
+			Expect(lastOperationResponse.Error).NotTo(HaveOccurred())
+			Expect(lastOperationResponse.StatusCode).To(Equal(http.StatusOK))
+			var receiver domain.LastOperation
+			err := json.Unmarshal(lastOperationResponse.ResponseBody, &receiver)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(receiver.State).NotTo(Equal("failed"))
+			return receiver.State == "succeeded"
+		}, time.Minute*2, time.Second*10).Should(BeTrue())
+	})
+})
