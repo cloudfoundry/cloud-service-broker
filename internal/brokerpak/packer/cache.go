6518b749e2da274b22ad770109592b8c88d1fe3a
Jatin Naik <jatinn@vmware.com>
2022-03-10 11:56:52 +0000

    chore: speedup pak command in tests (#413)
    added caching which can be turned on by setting an enviornment variable on local
    to speedup your local dev loops while running pak
    
    [#181404983]
    
    Signed-off-by: James Norman <normanja@vmware.com>

---
+++ internal/brokerpak/packer/cache.go
@@ -0,0 +1,56 @@
+package packer
+
+import (
+	"crypto/md5"
+	"fmt"
+	"log"
+	"os"
+	"path"
+
+	cp "github.com/otiai10/copy"
+)
+
+func copyFromCache(cachePath string, source string, destination string) bool {
+	if cachePath == "" {
+		return false
+	}
+	cacheKey := buildCacheKey(cachePath, source)
+	if _, err := os.Stat(cacheKey); err == nil {
+		cp.Copy(cacheKey, destination)
+		log.Println("\t", source, "found in cache at", cacheKey)
+		return true
+	} else {
+		return false
+	}
+}
+
+func populateCache(cachePath string, source string, destination string) {
+	if cachePath == "" {
+		return
+	}
+	cacheKey := buildCacheKey(cachePath, source)
+	err := cp.Copy(destination, cacheKey)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func buildCacheKey(cachePath string, source string) string {
+	return path.Join(cachePath, fmt.Sprintf("%x", md5.Sum([]byte(source))))
+}
+
+func cachedFetchFile(getter func(source string, destination string) error, source, destination, cachePath string) error {
+	if cachePath == "" {
+		return getter(source, destination)
+	}
+
+	if copyFromCache(cachePath, source, destination) {
+		return nil
+	}
+	err := getter(source, destination)
+	if err != nil {
+		return err
+	}
+	populateCache(cachePath, source, destination)
+	return nil
+}
