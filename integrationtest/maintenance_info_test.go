5df42e95975d99364a1a8cf0ccba1c86fa94081a
James Norman <normanja@vmware.com>
2022-05-19 09:47:20 +0100

    feat: maintenance info set as default terraform version (#493)
    * feat: maintenance info set as default terraform version
    
    [#182208672](https://www.pivotaltracker.com/story/show/182208672)
    
    * allow MI to be feature flagged
    
    [#182208672](https://www.pivotaltracker.com/story/show/182208672)
    
    * update test description
    
    [#182208672](https://www.pivotaltracker.com/story/show/182208672)
    
    * clean up definition test and decouple toPlan from executor
    
    [#182208672](https://www.pivotaltracker.com/story/show/182208672)
    
    * assert against JSON correctly
    
    [#182208672](https://www.pivotaltracker.com/story/show/182208672)

---
+++ integrationtest/maintenance_info_test.go
@@ -0,0 +1,69 @@
+package integrationtest_test
+
+import (
+	"encoding/json"
+
+	"github.com/pivotal-cf/brokerapi/v8/domain"
+
+	"github.com/pivotal-cf/brokerapi/v8/domain/apiresponses"
+
+	"github.com/cloudfoundry/cloud-service-broker/integrationtest/helper"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	. "github.com/onsi/gomega/gexec"
+)
+
+var _ = Describe("MaintenanceInfo", func() {
+	var (
+		testHelper *helper.TestHelper
+		session    *Session
+	)
+
+	BeforeEach(func() {
+		testHelper = helper.New(csb)
+		testHelper.BuildBrokerpak(testHelper.OriginalDir, "fixtures", "brokerpak-terraform-mi")
+	})
+
+	AfterEach(func() {
+		session.Terminate()
+		testHelper.Restore()
+	})
+
+	Context("Maintenance info", func() {
+		When("TF upgrades are enabled", func() {
+			BeforeEach(func() {
+				session = testHelper.StartBroker("TERRAFORM_UPGRADES_ENABLED=true")
+			})
+
+			It("should match the default Terraform version", func() {
+				catalogResponse := testHelper.Client().Catalog(requestID())
+				Expect(catalogResponse.Error).NotTo(HaveOccurred())
+
+				var catServices apiresponses.CatalogResponse
+				err := json.Unmarshal(catalogResponse.ResponseBody, &catServices)
+				Expect(err).NotTo(HaveOccurred())
+				Expect(catServices.Services[0].Plans[0].MaintenanceInfo).To(Equal(&domain.MaintenanceInfo{
+					Public:      nil,
+					Private:     "",
+					Version:     "0.13.7",
+					Description: "This upgrade provides support for Terraform version: 0.13.7. The upgrade operation will take a while. The instance and all associated bindings will be upgraded.",
+				}))
+			})
+		})
+
+		When("TF upgrades are disabled", func() {
+			BeforeEach(func() {
+				session = testHelper.StartBroker()
+			})
+
+			It("should not be set for the plan", func() {
+				catalogResponse := testHelper.Client().Catalog(requestID())
+
+				Expect(catalogResponse.Error).NotTo(HaveOccurred())
+				Expect(string(catalogResponse.ResponseBody)).ToNot(ContainSubstring(`"maintenance_info"`))
+			})
+		})
+
+	})
+
+})
