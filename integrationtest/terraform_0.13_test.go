e7cdd07d2cdd4effeafdd657697d803413799650
George Blue <gblue@pivotal.io>
2022-05-20 12:39:43 +0100

    test: de-duplicate operation polling logic
    Most integration tests poll the last_operation endpoint, sometimes using
    one of many duplicate helpers, and sometimes with duplicate code. This
    change results in all the tests using a single helper.

--- integrationtest/terraform_0.13_test.go
+++ integrationtest/terraform_0.13_test.go
@@ -1,16 +1,9 @@
 package integrationtest_test
 
 import (
-	"encoding/json"
-	"net/http"
-	"time"
-
 	"github.com/cloudfoundry/cloud-service-broker/integrationtest/helper"
 	. "github.com/onsi/ginkgo/v2"
-	. "github.com/onsi/gomega"
 	. "github.com/onsi/gomega/gexec"
-	"github.com/pborman/uuid"
-	"github.com/pivotal-cf/brokerapi/v8/domain"
 )
 
 var _ = Describe("Terraform 0.13", func() {
@@ -33,20 +26,6 @@ var _ = Describe("Terraform 0.13", func() {
 	It("can provision using this Terraform version", func() {
 		const serviceOfferingGUID = "df2c1512-3013-11ec-8704-2fbfa9c8a802"
 		const servicePlanGUID = "e59773ce-3013-11ec-9bbb-9376b4f72d14"
-		serviceInstanceGUID := uuid.New()
-		provisionResponse := testHelper.Client().Provision(serviceInstanceGUID, serviceOfferingGUID, servicePlanGUID, uuid.New(), nil)
-		Expect(provisionResponse.Error).NotTo(HaveOccurred())
-		Expect(provisionResponse.StatusCode).To(Equal(http.StatusAccepted))
-
-		Eventually(func() bool {
-			lastOperationResponse := testHelper.Client().LastOperation(serviceInstanceGUID, requestID())
-			Expect(lastOperationResponse.Error).NotTo(HaveOccurred())
-			Expect(lastOperationResponse.StatusCode).To(Equal(http.StatusOK))
-			var receiver domain.LastOperation
-			err := json.Unmarshal(lastOperationResponse.ResponseBody, &receiver)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(receiver.State).NotTo(Equal("failed"))
-			return receiver.State == "succeeded"
-		}, time.Minute*2, time.Second*10).Should(BeTrue())
+		testHelper.Provision(serviceOfferingGUID, servicePlanGUID)
 	})
 })
