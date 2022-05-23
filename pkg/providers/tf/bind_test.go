1341f20943b6c881c2f321e0d09b745bda16b911
James Norman <normanja@vmware.com>
2022-05-23 14:26:08 +0100

    test: add provider bind test
    [#182112028](https://www.pivotaltracker.com/story/show/182112028)

---
+++ pkg/providers/tf/bind_test.go
@@ -0,0 +1,180 @@
+package tf_test
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf/workspace/workspacefakes"
+
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf/workspace"
+
+	"github.com/cloudfoundry/cloud-service-broker/internal/storage"
+	"github.com/cloudfoundry/cloud-service-broker/pkg/broker"
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf"
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf/executor"
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf/tffakes"
+	"github.com/cloudfoundry/cloud-service-broker/pkg/varcontext"
+	"github.com/cloudfoundry/cloud-service-broker/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Bind", func() {
+
+	const expectedTfID = "58363e43-1f92-4174-a3fc-501dd5ee9e04"
+
+	var (
+		fakeDeploymentManager *tffakes.FakeDeploymentManagerInterface
+		deployment            storage.TerraformDeployment
+		fakeInvokerBuilder    *tffakes.FakeTerraformInvokerBuilder
+		fakeDefaultInvoker    *tffakes.FakeTerraformInvoker
+		fakeLogger            = utils.NewLogger("test")
+		fakeServiceDefinition tf.TfServiceDefinitionV1
+		bindContext           *varcontext.VarContext
+		templateVars          = map[string]interface{}{"tf_id": expectedTfID, "username": "some-user"}
+
+		fakeTerraformWorkspace *workspacefakes.FakeWorkspace
+	)
+
+	BeforeEach(func() {
+		fakeInvokerBuilder = &tffakes.FakeTerraformInvokerBuilder{}
+		fakeTerraformWorkspace = &workspacefakes.FakeWorkspace{}
+
+		var err error
+		bindContext, err = varcontext.Builder().MergeMap(templateVars).Build()
+		Expect(err).NotTo(HaveOccurred())
+
+		template := `
+				variable username { type = string }
+				output username { value = var.username }
+				`
+
+		fakeServiceDefinition = tf.TfServiceDefinitionV1{
+			BindSettings: tf.TfServiceDefinitionV1Action{
+				PlanInputs: []broker.BrokerVariable{
+					{
+						FieldName: "bind",
+					},
+				},
+				Outputs: []broker.BrokerVariable{
+					{
+						FieldName: "bind-output",
+					},
+				},
+				Template:  template,
+				Templates: map[string]string{"first": template},
+			},
+		}
+		fakeDeploymentManager = &tffakes.FakeDeploymentManagerInterface{}
+		fakeDefaultInvoker = &tffakes.FakeTerraformInvoker{}
+
+		deployment = storage.TerraformDeployment{
+			ID:        expectedTfID,
+			Workspace: fakeTerraformWorkspace,
+		}
+	})
+
+	When("binding successfully created", func() {
+		It("returns the binding output", func() {
+			fakeDeploymentManager.CreateAndSaveDeploymentReturns(deployment, nil)
+			fakeDeploymentManager.GetTerraformDeploymentReturns(deployment, nil)
+			fakeDeploymentManager.OperationStatusReturns(true, "operation succeeded", nil)
+			fakeInvokerBuilder.VersionedTerraformInvokerReturns(fakeDefaultInvoker)
+			fakeDefaultInvoker.ApplyReturns(nil)
+			fakeTerraformWorkspace.OutputsReturns(map[string]interface{}{"username": "some-user"}, nil)
+
+			provider := tf.NewTerraformProvider(executor.TFBinariesContext{}, fakeInvokerBuilder, fakeLogger, fakeServiceDefinition, fakeDeploymentManager)
+
+			actualBindDetails, err := provider.Bind(context.TODO(), bindContext)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(actualBindDetails["username"]).To(Equal("some-user"))
+
+			By("checking the new saved deployment")
+			Expect(fakeDeploymentManager.CreateAndSaveDeploymentCallCount()).To(Equal(1))
+			actualTfID, actualWorkspace := fakeDeploymentManager.CreateAndSaveDeploymentArgsForCall(0)
+			Expect(actualTfID).To(Equal(expectedTfID))
+			Expect(actualWorkspace.Modules[0].Name).To(Equal("brokertemplate"))
+			Expect(actualWorkspace.Modules[0].Definition).To(Equal(fakeServiceDefinition.BindSettings.Template))
+			Expect(actualWorkspace.Modules[0].Definitions).To(Equal(fakeServiceDefinition.BindSettings.Templates))
+			Expect(actualWorkspace.Instances[0].Configuration).To(Equal(map[string]interface{}{"username": "some-user"}))
+			Expect(actualWorkspace.Transformer.ParameterMappings).To(Equal([]workspace.ParameterMapping{}))
+			Expect(actualWorkspace.Transformer.ParametersToRemove).To(Equal([]string{}))
+			Expect(actualWorkspace.Transformer.ParametersToAdd).To(Equal([]workspace.ParameterMapping{}))
+
+			By("checking that provision is marked as started")
+			Expect(fakeDeploymentManager.MarkOperationStartedCallCount()).To(Equal(1))
+			actualDeployment, actualOperationType := fakeDeploymentManager.MarkOperationStartedArgsForCall(0)
+			Expect(actualDeployment).To(Equal(deployment))
+			Expect(actualOperationType).To(Equal("provision"))
+
+			By("checking TF apply has been called")
+			Eventually(applyCallCount(fakeDefaultInvoker)).Should(Equal(1))
+			Eventually(operationWasFinishedForDeployment(fakeDeploymentManager)).Should(Equal(deployment))
+			Expect(operationWasFinishedWithError(fakeDeploymentManager)()).To(BeNil())
+		})
+	})
+
+	It("fails, when tfID is not provided", func() {
+		var err error
+		bindContext, err = varcontext.Builder().Build()
+		Expect(err).NotTo(HaveOccurred())
+
+		provider := tf.NewTerraformProvider(executor.TFBinariesContext{}, fakeInvokerBuilder, fakeLogger, fakeServiceDefinition, fakeDeploymentManager)
+
+		_, err = provider.Bind(context.TODO(), bindContext)
+
+		Expect(err.Error()).To(ContainSubstring(`missing value for key "tf_id"`))
+	})
+
+	It("fails, when workspace cannot be created", func() {
+		fakeServiceDefinition = tf.TfServiceDefinitionV1{
+			BindSettings: tf.TfServiceDefinitionV1Action{
+				Template:  "invalid template",
+				Templates: map[string]string{"first": "invalid template"},
+			},
+		}
+
+		provider := tf.NewTerraformProvider(executor.TFBinariesContext{}, fakeInvokerBuilder, fakeLogger, fakeServiceDefinition, fakeDeploymentManager)
+
+		_, err := provider.Bind(context.TODO(), bindContext)
+
+		Expect(err).To(MatchError(`error from provider bind: error creating workspace: :1,17-17: Invalid block definition; Either a quoted string block label or an opening brace ("{") is expected here.`))
+	})
+
+	It("fails, when it errors saving the deployment", func() {
+		fakeDeploymentManager.CreateAndSaveDeploymentReturns(storage.TerraformDeployment{}, errors.New("cant save now"))
+		provider := tf.NewTerraformProvider(executor.TFBinariesContext{}, fakeInvokerBuilder, fakeLogger, fakeServiceDefinition, fakeDeploymentManager)
+
+		_, err := provider.Bind(context.TODO(), bindContext)
+
+		Expect(err).To(MatchError("error from provider bind: terraform provider create failed: cant save now"))
+	})
+
+	It("fails, when unable to mark the operation as started", func() {
+		fakeDeploymentManager.CreateAndSaveDeploymentReturns(deployment, nil)
+		fakeDeploymentManager.MarkOperationStartedReturns(errors.New("couldnt do this now"))
+		provider := tf.NewTerraformProvider(executor.TFBinariesContext{}, fakeInvokerBuilder, fakeLogger, fakeServiceDefinition, fakeDeploymentManager)
+
+		_, err := provider.Bind(context.TODO(), bindContext)
+
+		Expect(err).To(MatchError("error from provider bind: error marking job started: couldnt do this now"))
+	})
+
+	It("returns the error in last operation, if terraform apply fails", func() {
+		fakeDeploymentManager.CreateAndSaveDeploymentReturns(deployment, nil)
+		fakeDeploymentManager.OperationStatusReturns(true, "operation failed", fmt.Errorf("terraform apply failed"))
+		fakeDeploymentManager.MarkOperationFinishedReturns(errors.New("couldnt do this now"))
+		fakeInvokerBuilder.VersionedTerraformInvokerReturns(fakeDefaultInvoker)
+		fakeDefaultInvoker.ApplyReturns(errors.New("some TF issue happened"))
+		provider := tf.NewTerraformProvider(executor.TFBinariesContext{}, fakeInvokerBuilder, fakeLogger, fakeServiceDefinition, fakeDeploymentManager)
+
+		_, err := provider.Bind(context.TODO(), bindContext)
+		Expect(err).To(MatchError("error waiting for result: terraform apply failed"))
+
+		By("checking TF apply has been called")
+		Eventually(operationWasFinishedForDeployment(fakeDeploymentManager)).Should(Equal(deployment))
+		Expect(operationWasFinishedWithError(fakeDeploymentManager)()).To(MatchError("some TF issue happened"))
+	})
+
+})
