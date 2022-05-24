720ce5d3ebeb803c403b63caf74a619bf977a1f0
Felisia Martini <fmartini@pivotal.io>
2022-05-24 14:25:25 +0100

    test: add pool instance test (#515)

---
+++ pkg/providers/tf/poll_instance_test.go
@@ -0,0 +1,32 @@
+package tf_test
+
+import (
+	"context"
+
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf"
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf/executor"
+	"github.com/cloudfoundry/cloud-service-broker/pkg/providers/tf/tffakes"
+	"github.com/cloudfoundry/cloud-service-broker/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("PollInstance", func() {
+	It("returns gets operation status", func() {
+		fakeDeploymentManager := &tffakes.FakeDeploymentManagerInterface{}
+		fakeInvokerBuilder := &tffakes.FakeTerraformInvokerBuilder{}
+		fakeLogger := utils.NewLogger("test")
+
+		fakeDeploymentManager.OperationStatusReturns(true, "LO message", nil)
+		provider := tf.NewTerraformProvider(executor.TFBinariesContext{}, fakeInvokerBuilder, fakeLogger, tf.TfServiceDefinitionV1{}, fakeDeploymentManager)
+
+		finished, message, err := provider.PollInstance(context.TODO(), "instance-guid")
+
+		Expect(err).NotTo(HaveOccurred())
+		Expect(finished).To(BeTrue())
+		Expect(message).To(Equal("LO message"))
+
+		Expect(fakeDeploymentManager.OperationStatusCallCount()).To(Equal(1))
+		Expect(fakeDeploymentManager.OperationStatusArgsForCall(0)).To(Equal("tf:instance-guid:"))
+	})
+})
