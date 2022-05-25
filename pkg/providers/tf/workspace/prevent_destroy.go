d3f60be10817a26d8c1a3527d26862f894b0b0f1
George Blue <gblue@pivotal.io>
2022-05-25 10:00:15 +0100

    chore: changes from code review

--- pkg/providers/tf/workspace/prevent_destroy.go
+++ pkg/providers/tf/workspace/prevent_destroy.go
@@ -15,16 +15,16 @@ const (
 )
 
 func (workspace *TerraformWorkspace) RemovePreventDestroy() error {
-	for i, m := range workspace.Modules {
+	for index, module := range workspace.Modules {
 		var err error
-		workspace.Modules[i].Definition, err = removePreventDestroy(m.Definition)
+		workspace.Modules[index].Definition, err = removePreventDestroy(module.Definition)
 		if err != nil {
-			return fmt.Errorf("HCL parse error for module %d definition: %w", i, err)
+			return fmt.Errorf("HCL parse error for module %d definition: %w", index, err)
 		}
-		for k, v := range workspace.Modules[i].Definitions {
-			workspace.Modules[i].Definitions[k], err = removePreventDestroy(v)
+		for definitionName, definitionContents := range workspace.Modules[index].Definitions {
+			workspace.Modules[index].Definitions[definitionName], err = removePreventDestroy(definitionContents)
 			if err != nil {
-				return fmt.Errorf("HCL parse error for module %d definition %q: %w", i, k, err)
+				return fmt.Errorf("HCL parse error for module %d definition %q: %w", index, definitionName, err)
 			}
 		}
 	}
@@ -32,12 +32,12 @@ func (workspace *TerraformWorkspace) RemovePreventDestroy() error {
 }
 
 func removePreventDestroy(input string) (string, error) {
-	f, diags := hclwrite.ParseConfig([]byte(input), "", hcl.Pos{Line: 1, Column: 1})
+	file, diags := hclwrite.ParseConfig([]byte(input), "", hcl.Pos{Line: 1, Column: 1})
 	if diags.HasErrors() {
 		return "", diags
 	}
 
-	for _, resource := range f.Body().Blocks() {
+	for _, resource := range file.Body().Blocks() {
 		if resource.Type() == resourceIdentifier {
 			if lifecycle := resource.Body().FirstMatchingBlock(lifecycleIdentifier, nil); lifecycle != nil {
 				if pd := lifecycle.Body().GetAttribute(preventDestroyIdentifier); pd != nil {
@@ -47,5 +47,5 @@ func removePreventDestroy(input string) (string, error) {
 		}
 	}
 
-	return string(f.Bytes()), nil
+	return string(file.Bytes()), nil
 }
