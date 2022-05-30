02b36dc450024efb73ae3bd1de2f86e3d7731e69
Felisia Martini <fmartini@pivotal.io>
2022-05-30 11:24:25 +0100

    move other feature flag to central location
    [#182296561](https://www.pivotaltracker.com/story/show/182296561)
    
    Signed-off-by: James Norman <normanja@vmware.com>

--- pkg/featureflags/feature_flags.go
+++ pkg/featureflags/feature_flags.go
@@ -3,10 +3,14 @@ package featureflags
 import "github.com/spf13/viper"
 
 const (
-	TfUpgradeEnabled = "brokerpak.terraform.upgrades.enabled"
+	TfUpgradeEnabled  = "brokerpak.terraform.upgrades.enabled"
+	DynamicHCLEnabled = "brokerpak.updates.enabled"
 )
 
 func init() {
 	viper.BindEnv(TfUpgradeEnabled, "TERRAFORM_UPGRADES_ENABLED")
 	viper.SetDefault(TfUpgradeEnabled, false)
+
+	viper.BindEnv(DynamicHCLEnabled, "BROKERPAK_UPDATES_ENABLED")
+	viper.SetDefault(DynamicHCLEnabled, false)
 }
