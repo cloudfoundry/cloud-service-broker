881869867b2d5eeded9e00617fdd6d7bb975b707
George Blue <blgm@users.noreply.github.com>
2022-06-20 14:53:50 +0100

    test: consolodate upgrade integration tests (#542)
    * test: consolodate upgrade integration tests
    
    - there was a separate test for terraform 0.12 -> 0.13 and 0.13 -> 1.1
    - there was a separate test for instances with bindings and ones without
    - given the time taken to run a test, these tests have been merged
    - also added upgrade to Terraform 1.2 as this is now the latest
    
    [#181155154](https://www.pivotaltracker.com/story/show/181155154)
    
    * fix GH actions
    
    Co-authored-by: James Norman <normanja@vmware.com>

--- integrationtest/terraform_upgrade_test.go
+++ integrationtest/terraform_upgrade_test.go
@@ -1,11 +1,11 @@
 package integrationtest_test
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
-	"github.com/pborman/uuid"
-
+	"github.com/cloudfoundry/cloud-service-broker/dbservice/models"
 	"github.com/cloudfoundry/cloud-service-broker/integrationtest/helper"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -14,10 +14,39 @@ import (
 )
 
 var _ = Describe("Terraform Upgrade", func() {
+	const (
+		serviceOfferingGUID = "df2c1512-3013-11ec-8704-2fbfa9c8a802"
+		servicePlanGUID     = "e59773ce-3013-11ec-9bbb-9376b4f72d14"
+		startingVersion     = "0.12.21"
+		endingVersion       = "1.2.3"
+	)
+
 	var (
 		testHelper *helper.TestHelper
 		session    *Session
 	)
+
+	terraformStateOutputValue := func(deploymentID string, testHelper *helper.TestHelper) int {
+		var tfDeploymentReceiver models.TerraformDeployment
+		Expect(testHelper.DBConn().Where("id = ?", deploymentID).First(&tfDeploymentReceiver).Error).NotTo(HaveOccurred())
+		var workspaceReceiver struct {
+			State []byte `json:"tfstate"`
+		}
+		Expect(json.Unmarshal(tfDeploymentReceiver.Workspace, &workspaceReceiver)).NotTo(HaveOccurred())
+		var stateReceiver struct {
+			Outputs map[string]struct {
+				Type  string      `json:"type"`
+				Value interface{} `json:"value"`
+			} `json:"outputs"`
+		}
+		Expect(json.Unmarshal(workspaceReceiver.State, &stateReceiver)).NotTo(HaveOccurred())
+
+		var result float64
+		for _, value := range stateReceiver.Outputs {
+			result = value.Value.(float64)
+		}
+		return int(result)
+	}
 
 	instanceTerraformStateVersion := func(serviceInstanceGUID string) string {
 		return terraformStateVersion(fmt.Sprintf("tf:%s:", serviceInstanceGUID), testHelper)
@@ -38,21 +67,22 @@ var _ = Describe("Terraform Upgrade", func() {
 	BeforeEach(func() {
 		testHelper = helper.New(csb)
 		testHelper.BuildBrokerpak(testHelper.OriginalDir, "fixtures", "terraform-upgrade")
-
 		session = testHelper.StartBroker()
-	})
 
-	AfterEach(func() {
-		session.Terminate().Wait()
+		DeferCleanup(func() {
+			session.Terminate().Wait()
+		})
 	})
 
 	Context("TF Upgrades are enabled", func() {
 		It("runs 'terraform apply' at each version in the upgrade path", func() {
 			By("provisioning a service instance at 0.13")
-			const serviceOfferingGUID = "df2c1512-3013-11ec-8704-2fbfa9c8a802"
-			const servicePlanGUID = "e59773ce-3013-11ec-9bbb-9376b4f72d14"
 			serviceInstance := testHelper.Provision(serviceOfferingGUID, servicePlanGUID)
-			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal("0.13.7"))
+			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal(startingVersion))
+
+			By("creating service bindings")
+			firstBindGUID, _ := testHelper.CreateBinding(serviceInstance)
+			secondBindGUID, _ := testHelper.CreateBinding(serviceInstance)
 
 			By("updating the brokerpak and restarting the broker")
 			session.Terminate().Wait()
@@ -60,77 +90,36 @@ var _ = Describe("Terraform Upgrade", func() {
 
 			session = testHelper.StartBroker("TERRAFORM_UPGRADES_ENABLED=true")
 
+			By("validating old state")
+			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal(startingVersion))
+			Expect(instanceTerraformStateOutputValue(serviceInstance.GUID)).To(BeElementOf(1, 2))
+			Expect(bindingTerraformStateVersion(serviceInstance.GUID, firstBindGUID)).To(Equal(startingVersion))
+			Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, firstBindGUID)).To(BeElementOf(1, 2))
+			Expect(bindingTerraformStateVersion(serviceInstance.GUID, secondBindGUID)).To(Equal(startingVersion))
+			Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, secondBindGUID)).To(BeElementOf(1, 2))
+
 			By("running 'cf update-service'")
-			newMI := domain.MaintenanceInfo{
-				Version: "1.1.6",
-			}
-			testHelper.UpgradeService(serviceInstance, domain.PreviousValues{PlanID: servicePlanGUID}, newMI)
+			testHelper.UpgradeService(serviceInstance, domain.PreviousValues{PlanID: servicePlanGUID}, domain.MaintenanceInfo{Version: endingVersion})
 
-			By("observing that the TF state file has been updated to the latest version")
-			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal("1.1.6"))
-		})
+			By("observing that the instance TF state file has been updated to the latest version")
+			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal(endingVersion))
+			By("observing that the instance TF state file has updated output value")
+			Expect(instanceTerraformStateOutputValue(serviceInstance.GUID)).To(BeElementOf(3, 4))
 
-		When("the instance has bindings", func() {
-			It("runs 'terraform apply' at each version in the upgrade path", func() {
-				By("provisioning a service instance at 0.13")
-				const serviceOfferingGUID = "df2c1512-3013-11ec-8704-2fbfa9c8a802"
-				const servicePlanGUID = "e59773ce-3013-11ec-9bbb-9376b4f72d14"
-				serviceInstance := testHelper.Provision(serviceOfferingGUID, servicePlanGUID)
-				Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal("0.13.7"))
-
-				By("creating service bindings")
-				firstBindGUID := uuid.New()
-				firstBindResponse := testHelper.Client().Bind(serviceInstance.GUID, firstBindGUID, serviceOfferingGUID, servicePlanGUID, requestID(), nil)
-				Expect(firstBindResponse.Error).NotTo(HaveOccurred())
-				Expect(firstBindResponse.StatusCode).To(Equal(http.StatusCreated))
-
-				secondBindGUID := uuid.New()
-				secondBindResponse := testHelper.Client().Bind(serviceInstance.GUID, secondBindGUID, serviceOfferingGUID, servicePlanGUID, requestID(), nil)
-				Expect(secondBindResponse.Error).NotTo(HaveOccurred())
-				Expect(secondBindResponse.StatusCode).To(Equal(http.StatusCreated))
-
-				By("updating the brokerpak and restarting the broker")
-				session.Terminate().Wait()
-				testHelper.BuildBrokerpak(testHelper.OriginalDir, "fixtures", "terraform-upgrade-updated")
-
-				session = testHelper.StartBroker("TERRAFORM_UPGRADES_ENABLED=true")
-
-				By("validating old state")
-				Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal("0.13.7"))
-				Expect(instanceTerraformStateOutputValue(serviceInstance.GUID)).To(BeElementOf(1, 2))
-				Expect(bindingTerraformStateVersion(serviceInstance.GUID, firstBindGUID)).To(Equal("0.13.7"))
-				Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, firstBindGUID)).To(BeElementOf(1, 2))
-				Expect(bindingTerraformStateVersion(serviceInstance.GUID, secondBindGUID)).To(Equal("0.13.7"))
-				Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, secondBindGUID)).To(BeElementOf(1, 2))
-
-				By("running 'cf update-service'")
-				newMI := domain.MaintenanceInfo{
-					Version: "1.1.6",
-				}
-				testHelper.UpgradeService(serviceInstance, domain.PreviousValues{PlanID: servicePlanGUID}, newMI)
-
-				By("observing that the instance TF state file has been updated to the latest version")
-				Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal("1.1.6"))
-				By("observing that the instance TF state file has updated output value")
-				Expect(instanceTerraformStateOutputValue(serviceInstance.GUID)).To(BeElementOf(3, 4))
-
-				By("observing that the binding TF state file has been updated to the latest version")
-				Expect(bindingTerraformStateVersion(serviceInstance.GUID, firstBindGUID)).To(Equal("1.1.6"))
-				Expect(bindingTerraformStateVersion(serviceInstance.GUID, secondBindGUID)).To(Equal("1.1.6"))
-				By("observing that the binding TF state file has updated output value")
-				Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, firstBindGUID)).To(BeElementOf(3, 4))
-				Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, secondBindGUID)).To(BeElementOf(3, 4))
-			})
+			By("observing that the binding TF state file has been updated to the latest version")
+			Expect(bindingTerraformStateVersion(serviceInstance.GUID, firstBindGUID)).To(Equal(endingVersion))
+			Expect(bindingTerraformStateVersion(serviceInstance.GUID, secondBindGUID)).To(Equal(endingVersion))
+			By("observing that the binding TF state file has updated output value")
+			Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, firstBindGUID)).To(BeElementOf(3, 4))
+			Expect(bindingTerraformStateOutputValue(serviceInstance.GUID, secondBindGUID)).To(BeElementOf(3, 4))
 		})
 	})
 
 	Context("TF Upgrades are disabled", func() {
 		It("does not upgrade the instance", func() {
 			By("provisioning a service instance at 0.13")
-			const serviceOfferingGUID = "df2c1512-3013-11ec-8704-2fbfa9c8a802"
-			const servicePlanGUID = "e59773ce-3013-11ec-9bbb-9376b4f72d14"
 			serviceInstance := testHelper.Provision(serviceOfferingGUID, servicePlanGUID)
-			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal("0.13.7"))
+			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal(startingVersion))
 
 			By("updating the brokerpak and restarting the broker")
 			session.Terminate().Wait()
@@ -144,7 +133,21 @@ var _ = Describe("Terraform Upgrade", func() {
 			Expect(updateResponse.ResponseBody).To(ContainSubstring("terraform version check failed: operation attempted with newer version of Terraform than current state, upgrade the service before retrying operation"))
 
 			By("observing that the TF version remains the same in the state file")
-			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal("0.13.7"))
+			Expect(instanceTerraformStateVersion(serviceInstance.GUID)).To(Equal(startingVersion))
 		})
 	})
 })
+
+func terraformStateVersion(deploymentID string, testHelper *helper.TestHelper) string {
+	var tfDeploymentReceiver models.TerraformDeployment
+	Expect(testHelper.DBConn().Where("id = ?", deploymentID).First(&tfDeploymentReceiver).Error).NotTo(HaveOccurred())
+	var workspaceReceiver struct {
+		State []byte `json:"tfstate"`
+	}
+	Expect(json.Unmarshal(tfDeploymentReceiver.Workspace, &workspaceReceiver)).NotTo(HaveOccurred())
+	var stateReceiver struct {
+		Version string `json:"terraform_version"`
+	}
+	Expect(json.Unmarshal(workspaceReceiver.State, &stateReceiver)).NotTo(HaveOccurred())
+	return stateReceiver.Version
+}
