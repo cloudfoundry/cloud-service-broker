84e4d48939fd67762753fcf00732789dfb92e8c8
George Blue <blgm@users.noreply.github.com>
2022-06-22 15:59:20 +0100

    test: convert "decider" test to table test (#549)
    This is a conversion of the existing tests to a new format. It
    highlights some oddities with the previous tests (such as a duplicate),
    and a follow-on PR will resolve those.
    
    [#182522081](https://www.pivotaltracker.com/story/show/182522081)

--- brokerapi/broker/decider/decider_test.go
+++ brokerapi/broker/decider/decider_test.go
@@ -1,8 +1,8 @@
 package decider_test
 
 import (
-	"errors"
-	"net/http"
+	"fmt"
+	"regexp"
 
 	"github.com/cloudfoundry/cloud-service-broker/brokerapi/broker/decider"
 	"github.com/cloudfoundry/cloud-service-broker/internal/paramparser"
@@ -12,237 +12,69 @@ import (
 	"github.com/pivotal-cf/brokerapi/v8/domain/apiresponses"
 )
 
-var _ = Describe("Decider", func() {
-	const (
-		planWithMI         = "fake-plan-id-with-mi"
-		otherPlanWithMI    = "fake-other-plan-id-with-mi"
-		planWithoutMI      = "fake-plan-id-no-mi"
-		otherPlanWithoutMI = "fake-other-plan-id-no-mi"
-	)
-	var (
-		defaultMI *version.Version
-		higherMI  *version.Version
-	)
+var separator = regexp.MustCompile(`\s*;\s*`)
 
-	BeforeEach(func() {
-		defaultMI = version.Must(version.NewVersion("1.2.3"))
-		higherMI = version.Must(version.NewVersion("1.2.4"))
-	})
+var _ = DescribeTable(
+	"DecideOperation()",
+	func(spec string, expectedOperation decider.Operation, expectedError any) {
+		var (
+			details     paramparser.UpdateDetails
+			planVersion *version.Version
+		)
+		for _, token := range separator.Split(spec, -1) {
+			switch token {
+			case "no MI in plan", "no params", "no MI change", "no plan":
+			case "plan change":
+				details.PlanID = "new-plan-id"
+				details.PreviousPlanID = "previous-plan-id"
+			case "plan static":
+				details.PlanID = "static-plan-id"
+				details.PreviousPlanID = "static-plan-id"
+			case "no plan change":
+				details.PlanID = "static-plan-id"
+			case "params":
+				details.RequestParams = map[string]any{"foo": "bar"}
+			case "plan at v1":
+				planVersion = version.Must(version.NewVersion("1.0.0"))
+			case "MI none->v1":
+				details.MaintenanceInfoVersion = version.Must(version.NewVersion("1.0.0"))
+			case "MI none->v2":
+				details.MaintenanceInfoVersion = version.Must(version.NewVersion("2.0.0"))
+			case "MI v1->v1":
+				details.MaintenanceInfoVersion = version.Must(version.NewVersion("1.0.0"))
+				details.PreviousMaintenanceInfoVersion = version.Must(version.NewVersion("1.0.0"))
+			case "MI v1->none":
+				details.PreviousMaintenanceInfoVersion = version.Must(version.NewVersion("1.0.0"))
+			case "MI v2->v1":
+				details.MaintenanceInfoVersion = version.Must(version.NewVersion("1.0.0"))
+				details.PreviousMaintenanceInfoVersion = version.Must(version.NewVersion("2.0.0"))
+			default:
+				Fail(fmt.Sprintf("invalid token: %s", token))
+			}
+		}
 
-	Describe("DecideOperation", func() {
-		Context("request without maintenance_info", func() {
-			It("does not error when the catalog's plan doesn't have maintenance info either", func() {
-				details := paramparser.UpdateDetails{
-					PlanID:         otherPlanWithoutMI,
-					PreviousPlanID: planWithoutMI,
-				}
-
-				_, err := decider.DecideOperation(nil, details)
-				Expect(err).NotTo(HaveOccurred())
-			})
-
-			When("the request is a change of plan", func() {
-				It("is an update", func() {
-					details := paramparser.UpdateDetails{
-						PlanID:         otherPlanWithoutMI,
-						PreviousPlanID: planWithoutMI,
-					}
-
-					operation, err := decider.DecideOperation(nil, details)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(operation).To(Equal(decider.Update))
-				})
-			})
-
-			When("there are request parameters", func() {
-				It("is an update", func() {
-					details := paramparser.UpdateDetails{
-						PlanID:        planWithoutMI,
-						RequestParams: map[string]any{"foo": "bar"},
-					}
-
-					operation, err := decider.DecideOperation(nil, details)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(operation).To(Equal(decider.Update))
-				})
-			})
-
-			When("the plan does not change and there are no request parameters", func() {
-				It("is an update", func() {
-					details := paramparser.UpdateDetails{
-						PlanID:         planWithoutMI,
-						RequestParams:  map[string]any{},
-						PreviousPlanID: planWithoutMI,
-					}
-
-					operation, err := decider.DecideOperation(nil, details)
-					Expect(err).NotTo(HaveOccurred())
-					Expect(operation).To(Equal(decider.Update))
-				})
-			})
-
-			When("the desired plan has maintenance_info in the catalog", func() {
-				It("fails", func() {
-					details := paramparser.UpdateDetails{
-						PlanID: otherPlanWithMI,
-					}
-
-					operation, err := decider.DecideOperation(defaultMI, details)
-					Expect(err).To(MatchError("service instance needs to be upgraded before updating: maintenance info defined in broker service catalog, but not passed in request"))
-					Expect(operation).To(Equal(decider.Failed))
-				})
-			})
-		})
-
-		Context("request with maintenance_info", func() {
-			Context("request and plan have the same maintenance_info", func() {
-				When("the request is a change of plan", func() {
-					It("is an update", func() {
-						details := paramparser.UpdateDetails{
-							PlanID:                         otherPlanWithMI,
-							MaintenanceInfoVersion:         defaultMI,
-							PreviousPlanID:                 planWithMI,
-							PreviousMaintenanceInfoVersion: defaultMI,
-						}
-
-						operation, err := decider.DecideOperation(defaultMI, details)
-						Expect(err).NotTo(HaveOccurred())
-						Expect(operation).To(Equal(decider.Update))
-					})
-				})
-
-				When("there are request parameters", func() {
-					It("is an update", func() {
-						details := paramparser.UpdateDetails{
-							PlanID:                 planWithMI,
-							MaintenanceInfoVersion: defaultMI,
-							RequestParams:          map[string]any{"foo": "bar"},
-						}
-
-						operation, err := decider.DecideOperation(defaultMI, details)
-						Expect(err).NotTo(HaveOccurred())
-						Expect(operation).To(Equal(decider.Update))
-					})
-				})
-
-				When("the plan does not change and there are no request parameters", func() {
-					It("is an update", func() {
-						details := paramparser.UpdateDetails{
-							PlanID:                         planWithMI,
-							MaintenanceInfoVersion:         defaultMI,
-							RequestParams:                  map[string]any{},
-							PreviousPlanID:                 planWithMI,
-							PreviousMaintenanceInfoVersion: defaultMI,
-						}
-
-						operation, err := decider.DecideOperation(defaultMI, details)
-						Expect(err).NotTo(HaveOccurred())
-						Expect(operation).To(Equal(decider.Update))
-					})
-				})
-			})
-
-			Context("request has different maintenance_info values to the plan", func() {
-				When("adding maintenance_info when there was none before", func() {
-					It("is an upgrade", func() {
-						details := paramparser.UpdateDetails{
-							PlanID:                 planWithMI,
-							MaintenanceInfoVersion: defaultMI,
-							PreviousPlanID:         planWithMI,
-						}
-
-						operation, err := decider.DecideOperation(defaultMI, details)
-						Expect(err).NotTo(HaveOccurred())
-						Expect(operation).To(Equal(decider.Upgrade))
-					})
-				})
-
-				When("removing maintenance_info when it was there before", func() {
-					It("is an upgrade", func() {
-						details := paramparser.UpdateDetails{
-							PlanID:                         planWithoutMI,
-							PreviousPlanID:                 planWithoutMI,
-							PreviousMaintenanceInfoVersion: defaultMI,
-						}
-
-						operation, err := decider.DecideOperation(nil, details)
-						Expect(err).NotTo(HaveOccurred())
-						Expect(operation).To(Equal(decider.Upgrade))
-					})
-				})
-
-				When("the plan has not changed and there are no request parameters", func() {
-					It("is an upgrade", func() {
-						details := paramparser.UpdateDetails{
-							PlanID:                         planWithMI,
-							MaintenanceInfoVersion:         defaultMI,
-							PreviousPlanID:                 planWithMI,
-							PreviousMaintenanceInfoVersion: higherMI,
-						}
-
-						operation, err := decider.DecideOperation(defaultMI, details)
-						Expect(err).NotTo(HaveOccurred())
-						Expect(operation).To(Equal(decider.Upgrade))
-					})
-				})
-
-				When("there is a change of plan", func() {
-					It("is an update when the previous maintenance_info differs from the requested maintenance_info", func() {
-						details := paramparser.UpdateDetails{
-							PlanID:                         otherPlanWithMI,
-							MaintenanceInfoVersion:         higherMI,
-							PreviousPlanID:                 planWithMI,
-							PreviousMaintenanceInfoVersion: higherMI,
-						}
-
-						operation, err := decider.DecideOperation(higherMI, details)
-						Expect(err).NotTo(HaveOccurred())
-						Expect(operation).To(Equal(decider.Update))
-					})
-				})
-
-				When("there are request parameters", func() {
-					It("fails", func() {
-						details := paramparser.UpdateDetails{
-							RequestParams:                  map[string]any{"foo": "bar"},
-							PlanID:                         planWithMI,
-							MaintenanceInfoVersion:         defaultMI,
-							PreviousPlanID:                 planWithMI,
-							PreviousMaintenanceInfoVersion: higherMI,
-						}
-
-						_, err := decider.DecideOperation(defaultMI, details)
-						Expect(err).To(MatchError(apiresponses.NewFailureResponseBuilder(
-							errors.New("service instance needs to be upgraded before updating"),
-							http.StatusUnprocessableEntity,
-							"previous-maintenance-info-check",
-						).Build()))
-					})
-				})
-			})
-
-			Context("request and plan have different maintenance_info values", func() {
-				It("fails when the maintenance_info requested does not match the plan", func() {
-					details := paramparser.UpdateDetails{
-						PlanID:                 planWithMI,
-						MaintenanceInfoVersion: higherMI,
-					}
-
-					_, err := decider.DecideOperation(defaultMI, details)
-
-					Expect(err).To(MatchError(apiresponses.ErrMaintenanceInfoConflict))
-				})
-
-				It("fails when the request has maintenance_info but the plan does not", func() {
-					details := paramparser.UpdateDetails{
-						PlanID:                 planWithoutMI,
-						MaintenanceInfoVersion: defaultMI,
-					}
-
-					_, err := decider.DecideOperation(nil, details)
-					Expect(err).To(MatchError(apiresponses.ErrMaintenanceInfoNilConflict))
-				})
-			})
-		})
-	})
-})
+		operation, err := decider.DecideOperation(planVersion, details)
+		switch expectedError {
+		case nil:
+			Expect(err).NotTo(HaveOccurred())
+		default:
+			Expect(err).To(MatchError(expectedError))
+		}
+		Expect(operation).To(Equal(expectedOperation))
+	},
+	Entry(nil, "no MI in plan; no MI change; plan change;    no params", decider.Update, nil),
+	Entry(nil, "no MI in plan; no MI change; no plan change; params", decider.Update, nil),
+	Entry(nil, "no MI in plan; no MI change; plan static;    no params", decider.Update, nil),
+	Entry(nil, "plan at v1;    no MI change; plan static;    no params", decider.Failed, "service instance needs to be upgraded before updating: maintenance info defined in broker service catalog, but not passed in request"),
+	Entry(nil, "plan at v1;    MI v1->v1;    plan change;    no params", decider.Update, nil),
+	Entry(nil, "plan at v1;    MI none->v1;  no plan change; params", decider.Update, nil),
+	Entry(nil, "plan at v1;    MI v1->v1;    plan static;    no params", decider.Update, nil),
+	Entry(nil, "plan at v1;    MI none->v1;  plan static;    no params", decider.Upgrade, nil),
+	Entry(nil, "no MI in plan; MI v1->none;  plan static;    no params", decider.Upgrade, nil),
+	Entry(nil, "plan at v1;    MI v2->v1;    plan static;    no params", decider.Upgrade, nil),
+	Entry(nil, "plan at v1;    MI v1->v1;    plan change;    no params", decider.Update, nil), // Duplicate
+	Entry(nil, "plan at v1;    MI v2->v1;    plan static;    params", decider.Failed, "service instance needs to be upgraded before updating"),
+	Entry(nil, "plan at v1;    MI v2->v1;    plan static;    params", decider.Failed, "service instance needs to be upgraded before updating"),
+	Entry(nil, "plan at v1;    MI none->v2;  no plan change; no params", decider.Failed, apiresponses.ErrMaintenanceInfoConflict),
+	Entry(nil, "no MI in plan; MI none->v1;  no plan change; no params", decider.Failed, apiresponses.ErrMaintenanceInfoNilConflict),
+)
