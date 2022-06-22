c90bab859f6470dcafc67a82885d1b30f8418331
George Blue <blgm@users.noreply.github.com>
2022-06-22 08:21:54 +0100

    chore: update decider should used pre-parsed data (#547)
    We introduced `paramparser.ParseUpdateDetails()` to act as an
    abstraction layer between the `github.com/pivotal-cf/brokerapi`
    library and other code in the Cloud Service Broker. This stops
    brokerapi concepts and types from leaking across the whole codebase, and
    it allows us to shape the data into the form that is needed. The update
    decider did not follow this pattern, and it has now been updated to do
    so. This leads to considerable simplifiction of the update decider code.
    
    [#181155154](https://www.pivotaltracker.com/story/show/181155154)

--- internal/paramparser/parse_update_details_test.go
+++ internal/paramparser/parse_update_details_test.go
@@ -2,6 +2,7 @@ package paramparser_test
 
 import (
 	"github.com/cloudfoundry/cloud-service-broker/internal/paramparser"
+	"github.com/hashicorp/go-version"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/pivotal-cf/brokerapi/v8/domain"
@@ -10,28 +11,36 @@ import (
 var _ = Describe("ParseUpdateDetails", func() {
 	It("can parse update details", func() {
 		ud, err := paramparser.ParseUpdateDetails(domain.UpdateDetails{
-			ServiceID:     "fake-service-id",
-			PlanID:        "fake-plan-id",
+			ServiceID: "fake-service-id",
+			PlanID:    "fake-plan-id",
+			MaintenanceInfo: &domain.MaintenanceInfo{
+				Version: "1.2.3",
+			},
 			RawParameters: []byte(`{"foo":"bar"}`),
 			PreviousValues: domain.PreviousValues{
 				PlanID:    "fake-previous-plan-id",
 				ServiceID: "fake-previous-service-id",
 				OrgID:     "fake-previous-org-id",
 				SpaceID:   "fake-previous-space-id",
+				MaintenanceInfo: &domain.MaintenanceInfo{
+					Version: "0.1.2",
+				},
 			},
 			RawContext: []byte(`{"baz":"quz"}`),
 		})
 
 		Expect(err).NotTo(HaveOccurred())
 		Expect(ud).To(Equal(paramparser.UpdateDetails{
-			ServiceID:         "fake-service-id",
-			PlanID:            "fake-plan-id",
-			RequestParams:     map[string]interface{}{"foo": "bar"},
-			RequestContext:    map[string]interface{}{"baz": "quz"},
-			PreviousPlanID:    "fake-previous-plan-id",
-			PreviousServiceID: "fake-previous-service-id",
-			PreviousOrgID:     "fake-previous-org-id",
-			PreviousSpaceID:   "fake-previous-space-id",
+			ServiceID:                      "fake-service-id",
+			PlanID:                         "fake-plan-id",
+			MaintenanceInfoVersion:         version.Must(version.NewVersion("1.2.3")),
+			RequestParams:                  map[string]any{"foo": "bar"},
+			RequestContext:                 map[string]any{"baz": "quz"},
+			PreviousPlanID:                 "fake-previous-plan-id",
+			PreviousServiceID:              "fake-previous-service-id",
+			PreviousOrgID:                  "fake-previous-org-id",
+			PreviousSpaceID:                "fake-previous-space-id",
+			PreviousMaintenanceInfoVersion: version.Must(version.NewVersion("0.1.2")),
 		}))
 	})
 
@@ -57,9 +66,51 @@ var _ = Describe("ParseUpdateDetails", func() {
 		})
 	})
 
+	When("maintenance info version is not valid", func() {
+		It("returns an error", func() {
+			ud, err := paramparser.ParseUpdateDetails(domain.UpdateDetails{
+				MaintenanceInfo: &domain.MaintenanceInfo{
+					Version: "not-a-valid-version",
+				},
+			})
+
+			Expect(err).To(MatchError(`error parsing maintenance info: Malformed version: not-a-valid-version`))
+			Expect(ud).To(BeZero())
+		})
+	})
+
+	When("previous maintenance info version is not valid", func() {
+		It("returns an error", func() {
+			ud, err := paramparser.ParseUpdateDetails(domain.UpdateDetails{
+				PreviousValues: domain.PreviousValues{
+					MaintenanceInfo: &domain.MaintenanceInfo{
+						Version: "not-a-valid-version",
+					},
+				},
+			})
+
+			Expect(err).To(MatchError(`error parsing previous maintenance info: Malformed version: not-a-valid-version`))
+			Expect(ud).To(BeZero())
+		})
+	})
+
 	When("input is empty", func() {
 		It("succeeds with an empty result", func() {
 			ud, err := paramparser.ParseUpdateDetails(domain.UpdateDetails{})
+
+			Expect(err).NotTo(HaveOccurred())
+			Expect(ud).To(BeZero())
+		})
+	})
+
+	When("maintenance_info versions are empty", func() {
+		It("succeeds with an empty result", func() {
+			ud, err := paramparser.ParseUpdateDetails(domain.UpdateDetails{
+				MaintenanceInfo: &domain.MaintenanceInfo{},
+				PreviousValues: domain.PreviousValues{
+					MaintenanceInfo: &domain.MaintenanceInfo{},
+				},
+			})
 
 			Expect(err).NotTo(HaveOccurred())
 			Expect(ud).To(BeZero())
