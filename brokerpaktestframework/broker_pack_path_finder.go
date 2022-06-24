ce875b0823dea7fbb77aaf0646bc26876a27ead6
George Blue <gblue@pivotal.io>
2022-06-24 12:16:56 +0100

    fix: brokerpaktestframework fails with "terraform_upgrade_path"
    The brokerpaktestframwork failed if a "terraform_upgrade_path" was
    specified. This is because the framework used to update the manifest to
    have a single version of Terraform at 1.1.4. But when other versions are
    specified in the "terraform_upgrade_path" then the manifest validation
    fails.
    
    - The brokerpaktestframework now reads the Terraform versions present in
    the brokerpak and fakes them all (with the default version)
    - There is a test for the brokerpaktestframework which will hopefully
    find some of the worst errors in future
    - The signature of `FindServicePlanGUIDs()` has been changed to return
    an error rather than fail silentlu
    - The fake broker is now started on a random port so that it does not
    interfere with any locally running CSB instances
    - If building the fake brokerpak fails, an error is returned
    
    [#181155154](https://www.pivotaltracker.com/story/show/181155154)

--- brokerpaktestframework/broker_pack_path_finder.go
+++ brokerpaktestframework/broker_pack_path_finder.go
@@ -5,8 +5,16 @@ import (
 	"runtime"
 )
 
-func PathToBrokerPack() string {
-	_, file, _, _ := runtime.Caller(1)
+func PathToBrokerPack(skips ...int) string {
+	skip := 1
+	switch len(skips) {
+	case 0:
+	case 1:
+		skip += skips[0]
+	default:
+		panic("too many skips")
+	}
+	_, file, _, _ := runtime.Caller(skip)
 
 	return filepath.Dir(filepath.Dir(file))
 }
---
+++ integrationtest/brokerpaktestframework_test.go
@@ -0,0 +1,56 @@
+package integrationtest
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/cloudfoundry/cloud-service-broker/brokerpaktestframework"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("brokerpaktestframework", func() {
+	It("works", func() {
+		By("creating a mock Terraform")
+		mockTerraform, err := brokerpaktestframework.NewTerraformMock(brokerpaktestframework.WithVersion("1.2.3"))
+		Expect(err).NotTo(HaveOccurred())
+
+		By("building a fake brokerpak")
+		cwd, err := os.Getwd()
+		Expect(err).NotTo(HaveOccurred())
+		broker, err := brokerpaktestframework.BuildTestInstance(
+			filepath.Join(cwd, "fixtures", "brokerpaktestframework"),
+			mockTerraform,
+			GinkgoWriter,
+		)
+		Expect(err).NotTo(HaveOccurred())
+
+		By("starting the broker")
+		Expect(broker.Start(GinkgoWriter, nil)).To(Succeed())
+
+		By("testing catalog")
+		catalog, err := broker.Catalog()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(catalog.Services).To(HaveLen(1))
+		Expect(catalog.Services[0].Name).To(Equal("alpha-service"))
+
+		By("testing provision")
+		_, err = broker.Provision("does-not-exist", "does-not-exist", nil)
+		Expect(err).To(MatchError(`cannot find service "does-not-exist" and plan "does-not-exist" in catalog`))
+		serviceInstanceID, err := broker.Provision("alpha-service", "alpha", nil)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(serviceInstanceID).To(HaveLen(36))
+
+		By("testing bind")
+		_, err = broker.Bind("does-not-exist", "does-not-exist", "does-not-exist", nil)
+		Expect(err).To(MatchError(`cannot find service "does-not-exist" and plan "does-not-exist" in catalog`))
+		_, err = broker.Bind("alpha-service", "alpha", "does-not-exist", nil)
+		Expect(err).To(MatchError(ContainSubstring(`error retrieving service instance details: could not find service instance details for: does-not-exist`)))
+		creds, err := broker.Bind("alpha-service", "alpha", serviceInstanceID, nil)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(creds).To(BeEmpty())
+
+		By("stopping the broker")
+		Expect(broker.Cleanup()).To(Succeed())
+	})
+})
