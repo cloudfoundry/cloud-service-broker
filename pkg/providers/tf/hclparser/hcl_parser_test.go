796f8172440cd3ac60e4310740372b10c82f11bb
George Blue <blgm@users.noreply.github.com>
2022-10-10 10:37:10 +0100

    fix: panic when looking up tf_attribute values (#622)
    If a tf_attribute pointed to a resource that didn't exist then a useful
    error occurred. But if the resource existed but the attribute did not,
    then a panic occurred.
    
    [#183463447](https://www.pivotaltracker.com/story/show/183463447)
    
    Co-authored-by: Andrea Zucchini <zandrea@vmware.com>

--- pkg/providers/tf/hclparser/hcl_parser_test.go
+++ pkg/providers/tf/hclparser/hcl_parser_test.go
@@ -6,92 +6,106 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("HclParser", func() {
-	Describe("GetSubsumedParams", func() {
-		When("TF HCL contains all replace properties", func() {
-			It("succeeds", func() {
-				replaceVars := []hclparser.ExtractVariable{
-					{
-						FieldToRead:  "resource_type.resource_name.subsume_key",
-						FieldToWrite: "field_to_replace",
-					},
-					{
-						FieldToRead:  "other_resource_type.resource_name.other_subsume_key",
-						FieldToWrite: "other_field_to_replace",
-					},
-				}
-				tfHCL := "# resource_type.resource_name:\nresource \"resource_type\" \"resource_name\" {\nsubsume_key = \"subsume_value\"\n}" +
-					" \n# other_resource_type.resource_name:\nresource \"other_resource_type\" \"resource_name\" {\nother_subsume_key = \"other_subsume_value\"\n} " +
-					" \nOutputs:\nname = \"test-name\""
+var _ = Describe("GetParameters", func() {
+	When("TF HCL contains all replace properties", func() {
+		It("succeeds", func() {
+			replaceVars := []hclparser.ExtractVariable{
+				{
+					FieldToRead:  "resource_type.resource_name.subsume_key",
+					FieldToWrite: "field_to_replace",
+				},
+				{
+					FieldToRead:  "other_resource_type.resource_name.other_subsume_key",
+					FieldToWrite: "other_field_to_replace",
+				},
+			}
+			tfHCL := "# resource_type.resource_name:\nresource \"resource_type\" \"resource_name\" {\nsubsume_key = \"subsume_value\"\n}" +
+				" \n# other_resource_type.resource_name:\nresource \"other_resource_type\" \"resource_name\" {\nother_subsume_key = \"other_subsume_value\"\n} " +
+				" \nOutputs:\nname = \"test-name\""
 
-				output, err := hclparser.GetParameters(tfHCL, replaceVars)
+			output, err := hclparser.GetParameters(tfHCL, replaceVars)
 
-				Expect(err).NotTo(HaveOccurred())
-				Expect(output).To(Equal(map[string]any{
-					"field_to_replace":       "subsume_value",
-					"other_field_to_replace": "other_subsume_value",
-				}))
-			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(Equal(map[string]any{
+				"field_to_replace":       "subsume_value",
+				"other_field_to_replace": "other_subsume_value",
+			}))
 		})
+	})
 
-		When("TF HCL does not have outputs block", func() {
-			It("succeeds", func() {
-				replaceVars := []hclparser.ExtractVariable{
-					{
-						FieldToRead:  "resource_type.resource_name.subsume_key",
-						FieldToWrite: "field_to_replace",
-					},
-				}
-				tfHCL := "# resource_type.resource_name:\nresource \"resource_type\" \"resource_name\" {\nsubsume_key = \"subsume_value\"\n}"
+	When("TF HCL does not have outputs block", func() {
+		It("succeeds", func() {
+			replaceVars := []hclparser.ExtractVariable{
+				{
+					FieldToRead:  "resource_type.resource_name.subsume_key",
+					FieldToWrite: "field_to_replace",
+				},
+			}
+			tfHCL := "# resource_type.resource_name:\nresource \"resource_type\" \"resource_name\" {\nsubsume_key = \"subsume_value\"\n}"
 
-				output, err := hclparser.GetParameters(tfHCL, replaceVars)
+			output, err := hclparser.GetParameters(tfHCL, replaceVars)
 
-				Expect(err).NotTo(HaveOccurred())
-				Expect(output).To(Equal(map[string]any{"field_to_replace": "subsume_value"}))
-			})
+			Expect(err).NotTo(HaveOccurred())
+			Expect(output).To(Equal(map[string]any{"field_to_replace": "subsume_value"}))
 		})
+	})
 
-		When("TF HCL does not contain one of the replace vars", func() {
-			It("fails", func() {
-				replaceVars := []hclparser.ExtractVariable{
-					{
-						FieldToRead:  "resource_type.resource_name.subsume_key",
-						FieldToWrite: "field_to_replace",
-					},
-					{
-						FieldToRead:  "other_resource_type.resource_name.other_subsume_key",
-						FieldToWrite: "other_field_to_replace",
-					},
-				}
-				tfHCL := "\n# other_resource_type.resource_name:\nresource \"other_resource_type\" \"resource_name\" {\nother_subsume_key = \"other_subsume_value\"\n}\nOutputs:\nname = \"test-name\""
+	When("TF HCL does not contain one of the replace var resources", func() {
+		It("fails", func() {
+			replaceVars := []hclparser.ExtractVariable{
+				{
+					FieldToRead:  "resource_type.resource_name.subsume_key",
+					FieldToWrite: "field_to_replace",
+				},
+				{
+					FieldToRead:  "other_resource_type.resource_name.other_subsume_key",
+					FieldToWrite: "other_field_to_replace",
+				},
+			}
+			tfHCL := "\n# other_resource_type.resource_name:\nresource \"other_resource_type\" \"resource_name\" {\nother_subsume_key = \"other_subsume_value\"\n}\nOutputs:\nname = \"test-name\""
 
-				_, err := hclparser.GetParameters(tfHCL, replaceVars)
+			_, err := hclparser.GetParameters(tfHCL, replaceVars)
 
-				Expect(err).To(MatchError("cannot find required subsumed values for fields: resource_type.resource_name.subsume_key"))
-			})
+			Expect(err).To(MatchError("cannot find required subsumed values for fields: resource_type.resource_name.subsume_key"))
 		})
+	})
 
-		When("TF HCL is empty", func() {
-			It("fails", func() {
-				replaceVars := []hclparser.ExtractVariable{
-					{
-						FieldToRead:  "resource_type.resource_name.subsume_key",
-						FieldToWrite: "field_to_replace",
-					},
-				}
+	When("TF HCL does not contain one of the replace var attributes", func() {
+		It("fails", func() {
+			replaceVars := []hclparser.ExtractVariable{
+				{
+					FieldToRead:  "other_resource_type.resource_name.non_existing_key",
+					FieldToWrite: "other_field_to_replace",
+				},
+			}
+			tfHCL := "\n# other_resource_type.resource_name:\nresource \"other_resource_type\" \"resource_name\" {\nother_subsume_key = \"other_subsume_value\"\n}\nOutputs:\nname = \"test-name\""
 
-				_, err := hclparser.GetParameters("", replaceVars)
+			_, err := hclparser.GetParameters(tfHCL, replaceVars)
 
-				Expect(err).To(MatchError("cannot find required subsumed values for fields: resource_type.resource_name.subsume_key"))
-			})
+			Expect(err).To(MatchError("cannot find required subsumed values for fields: other_resource_type.resource_name.non_existing_key"))
 		})
+	})
 
-		When("TF HCL cannot be parsed", func() {
-			It("fails", func() {
-				_, err := hclparser.GetParameters("not valid", []hclparser.ExtractVariable{})
+	When("TF HCL is empty", func() {
+		It("fails", func() {
+			replaceVars := []hclparser.ExtractVariable{
+				{
+					FieldToRead:  "resource_type.resource_name.subsume_key",
+					FieldToWrite: "field_to_replace",
+				},
+			}
 
-				Expect(err).To(MatchError(ContainSubstring("error parsing subsumed HCL file:")))
-			})
+			_, err := hclparser.GetParameters("", replaceVars)
+
+			Expect(err).To(MatchError("cannot find required subsumed values for fields: resource_type.resource_name.subsume_key"))
+		})
+	})
+
+	When("TF HCL cannot be parsed", func() {
+		It("fails", func() {
+			_, err := hclparser.GetParameters("not valid", []hclparser.ExtractVariable{})
+
+			Expect(err).To(MatchError(ContainSubstring("error parsing subsumed HCL file:")))
 		})
 	})
 })
