59254d5bfbc3e4b706f4de29b340dbd5ee9f3363
George Blue <blgm@users.noreply.github.com>
2023-02-13 13:34:24 +0000

    feat!: remove legacy client run-examples command (#729)
    - New "csb run-examples" and "csb examples" commands have been
    introduced and we have got good feedback on them
    - This commit removes the "csb client run-examples" and
    "csb client examples" commands since we believe they are now superfluous
    - The commands can be added back by setting the feature flag
    `CSB_ENABLE_LEGACY_EXAMPLES_COMMANDS=1`, but if you do this,
    please raise an issue to let us know as otherwise we will remove
    the functionality completely in a future release
    
    Co-authored-by: Andrea Zucchini <zandrea@vmware.com>

--- pkg/featureflags/feature_flags.go
+++ pkg/featureflags/feature_flags.go
@@ -9,13 +9,20 @@ const (
 	TfUpgradeEnabled                 FeatureFlagName = "brokerpak.terraform.upgrades.enabled"
 	DynamicHCLEnabled                FeatureFlagName = "brokerpak.updates.enabled"
 	DisableRequestPropertyValidation FeatureFlagName = "request.property.validation.disabled"
+
+	// EnableLegacyExamplesCommands enabled the old way of running example tests
+	// IF YOU USE THIS, PLEASE RAISE AN ISSUE. Since the new way of running examples was added,
+	// the authors don't expect anyone to use the legacy method. Please let us know if you need
+	// the old way to stay around.
+	EnableLegacyExamplesCommands FeatureFlagName = "legacy.examples.enabled"
 )
 
 func init() {
 	for ffName, varName := range map[FeatureFlagName]string{
-		TfUpgradeEnabled:                 "TERRAFORM_UPGRADES_ENABLED",
-		DynamicHCLEnabled:                "BROKERPAK_UPDATES_ENABLED",
+		TfUpgradeEnabled:                 "TERRAFORM_UPGRADES_ENABLED", // deprecated pattern - future variables should start CSB_
+		DynamicHCLEnabled:                "BROKERPAK_UPDATES_ENABLED",  // deprecated pattern - future variables should start CSB_
 		DisableRequestPropertyValidation: "CSB_DISABLE_REQUEST_PROPERTY_VALIDATION",
+		EnableLegacyExamplesCommands:     "CSB_ENABLE_LEGACY_EXAMPLES_COMMANDS",
 	} {
 		viper.BindEnv(string(ffName), varName)
 		viper.SetDefault(string(ffName), false)
