3b7f4f129a4992aa7af9a55d905190cd81a254f7
George Blue <blgm@users.noreply.github.com>
2025-05-01 16:34:56 +0100

    test: CredHub CA Cert and TLS skip (#1239)

--- internal/brokerchapi/brokerchapi_suite_test.go
+++ internal/brokerchapi/brokerchapi_suite_test.go
@@ -1,13 +1,35 @@
 package brokerchapi_test
 
 import (
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
+	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/gexec"
 )
 
 func TestBrokerCHAPI(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Broker CredHub API Suite")
 }
+
+var fakeCertsPath string
+
+var _ = SynchronizedBeforeSuite(
+	func() []byte {
+		path := GinkgoT().TempDir()
+
+		cmd := exec.Command("bash", filepath.Join(must(os.Getwd()), "generateFakeCerts.sh"))
+		cmd.Dir = path
+		session := must(gexec.Start(cmd, GinkgoWriter, GinkgoWriter))
+		Eventually(session).WithTimeout(time.Minute).WithPolling(time.Second).Should(gexec.Exit(0))
+		return []byte(path)
+	},
+	func(input []byte) {
+		fakeCertsPath = string(input)
+	},
+)
